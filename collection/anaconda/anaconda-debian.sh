e80484c16996a3c337ea32916b60beab11d53ed1
Josh Spicer <josh@joshspicer.com>
2022-05-09 18:47:43 +0000

    init

---
+++ collection/anaconda/anaconda-debian.sh
@@ -0,0 +1,99 @@
+#!/usr/bin/env bash
+#-------------------------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See https://go.microsoft.com/fwlink/?linkid=2090316 for license information.
+#-------------------------------------------------------------------------------------------------------------
+#
+# Docs: https://github.com/microsoft/vscode-dev-containers/blob/main/script-library/docs/anaconda.md
+# Maintainer: The VS Code and Codespaces Teams
+#
+# Syntax: ./anaconda-debian.sh [Conda version] [CONDA_DIR] [Non-root user] [Add rc files flag]
+
+VERSION=${1:-"latest"}
+export CONDA_DIR=${2:-"/usr/local/conda"}
+USERNAME=${3:-"automatic"}
+UPDATE_RC=${4:-"true"}
+
+set -eux
+export DEBIAN_FRONTEND=noninteractive
+
+if [ "$(id -u)" -ne 0 ]; then
+    echo -e 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
+    exit 1
+fi
+
+# Ensure that login shells get the correct path if the user updated the PATH using ENV.
+rm -f /etc/profile.d/00-restore-env.sh
+echo "export PATH=${PATH//$(sh -lc 'echo $PATH')/\$PATH}" > /etc/profile.d/00-restore-env.sh
+chmod +x /etc/profile.d/00-restore-env.sh
+
+# Determine the appropriate non-root user
+if [ "${USERNAME}" = "auto" ] || [ "${USERNAME}" = "automatic" ]; then
+    USERNAME=""
+    POSSIBLE_USERS=("vscode" "node" "codespace" "$(awk -v val=1000 -F ":" '$3==val{print $1}' /etc/passwd)")
+    for CURRENT_USER in ${POSSIBLE_USERS[@]}; do
+        if id -u ${CURRENT_USER} > /dev/null 2>&1; then
+            USERNAME=${CURRENT_USER}
+            break
+        fi
+    done
+    if [ "${USERNAME}" = "" ]; then
+        USERNAME=root
+    fi
+elif [ "${USERNAME}" = "none" ] || ! id -u ${USERNAME} > /dev/null 2>&1; then
+    USERNAME=root
+fi
+
+architecture="$(uname -m)"
+if [ "${architecture}" != "x86_64" ]; then
+    echo "(!) Architecture $architecture unsupported"
+    exit 1
+fi
+
+updaterc() {
+    if [ "${UPDATE_RC}" = "true" ]; then
+        echo "Updating /etc/bash.bashrc and /etc/zsh/zshrc..."
+        if [[ "$(cat /etc/bash.bashrc)" != *"$1"* ]]; then
+            echo -e "$1" >> /etc/bash.bashrc
+        fi
+        if [ -f "/etc/zsh/zshrc" ] && [[ "$(cat /etc/zsh/zshrc)" != *"$1"* ]]; then
+            echo -e "$1" >> /etc/zsh/zshrc
+        fi
+    fi
+}
+
+# Checks if packages are installed and installs them if not
+check_packages() {
+    if ! dpkg -s "$@" > /dev/null 2>&1; then
+        apt-get update
+        apt-get -y install --no-install-recommends "$@"
+    fi
+}
+
+# Install Conda if it's missing
+if ! conda --version &> /dev/null ; then
+    # Install dependencies
+    check_packages wget ca-certificates
+
+    mkdir -p $CONDA_DIR
+    chown ${USERNAME}:root $CONDA_DIR
+    echo "Installing Anaconda..."
+
+    CONDA_VERSION=$VERSION
+    if [ "${VERSION}" = "latest" ] || [ "${VERSION}" = "lts" ]; then
+        CONDA_VERSION="2021.11"
+    fi
+
+    su --login -c "wget -q https://repo.anaconda.com/archive/Anaconda3-${CONDA_VERSION}-Linux-x86_64.sh -O /tmp/anaconda-install.sh \
+        && /bin/bash /tmp/anaconda-install.sh -u -b -p ${CONDA_DIR}" ${USERNAME} 2>&1 
+    
+    if [ "${VERSION}" = "latest" ] || [ "${VERSION}" = "lts" ]; then
+        PATH=$PATH:${CONDA_DIR}/bin
+        conda update -y conda
+    fi
+
+    rm /tmp/anaconda-install.sh 
+    updaterc "export CONDA_DIR=${CONDA_DIR}/bin"
+fi
+
+echo "Done!"
---
+++ collection/apt/run.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+apt-get update
+apt-get install -yq $COMMAND
