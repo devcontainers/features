2eab30c796679466d35661941f772d36b2d6b239
Samruddhi Khandale <skhandale@microsoft.com>
2022-05-16 18:40:21 +0000

    move contents

--- src/common/install.sh
+++ src/common/install.sh
@@ -369,7 +369,7 @@ if [ "${INSTALL_ZSH}" = "true" ]; then
         apt-get install -y zsh
     fi
     if [ "${ZSH_ALREADY_INSTALLED}" != "true" ]; then
-        echo "${rc_snippet}" >> /etc/zsh/zshrc
+        echo "${rc_snippet}" > /etc/zsh/zshrc
         ZSH_ALREADY_INSTALLED="true"
     fi
 
@@ -450,5 +450,23 @@ echo -e "\
     EXISTING_NON_ROOT_USER=${EXISTING_NON_ROOT_USER}\n\
     RC_SNIPPET_ALREADY_ADDED=${RC_SNIPPET_ALREADY_ADDED}\n\
     ZSH_ALREADY_INSTALLED=${ZSH_ALREADY_INSTALLED}" > "${MARKER_FILE}"
+
+# Display a notice on conda when not running in GitHub Codespaces
+cat << 'EOF' > /usr/local/etc/vscode-dev-containers/conda-notice.txt
+When using "conda" from outside of GitHub Codespaces, note the Anaconda repository contains
+restrictions on commercial use that may impact certain organizations. See https://aka.ms/ghcs-conda
+
+EOF
+
+notice_script="$(cat << 'EOF'
+if [ -t 1 ] && [ "${IGNORE_NOTICE}" != "true" ] && [ "${TERM_PROGRAM}" = "vscode" ] && [ "${CODESPACES}" != "true" ] && [ ! -f "$HOME/.config/vscode-dev-containers/conda-notice-already-displayed" ]; then
+    cat "/usr/local/etc/vscode-dev-containers/conda-notice.txt"
+    mkdir -p "$HOME/.config/vscode-dev-containers"
+    ((sleep 10s; touch "$HOME/.config/vscode-dev-containers/conda-notice-already-displayed") &)
+fi
+EOF
+)"
+
+echo "${notice_script}" | tee -a /etc/bash.bashrc >> /etc/zsh/zshrc
 
 echo "Done!"
