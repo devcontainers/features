bbc990c2328dfb2e982b8db4fd9ce9ac6ecf403a
Josh Spicer <joshspicer@github.com>
2022-05-25 14:19:33 -0700

    format test/git/test.sh

--- test/git/test.sh
+++ test/git/test.sh
@@ -1,13 +1,12 @@
+#!/bin/bash
 
-    #!/bin/bash
+set -e
 
-    set -e
+# Optional: Import test library
+source dev-container-features-test-lib
 
-    # Optional: Import test library
-    source dev-container-features-test-lib
+# Definition specific tests
+check "version" git  --version
 
-    # Definition specific tests
-    check "version" git  --version
-
-    # Report result
-    reportResults
+# Report result
+reportResults
