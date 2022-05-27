373a1155143780fd261fbf45c7180ffee9e274b6
Samruddhi Khandale <skhandale@microsoft.com>
2022-05-27 16:43:14 +0000

    install with another approach

--- src/oryx/install.sh
+++ src/oryx/install.sh
@@ -76,9 +76,11 @@ install_dotnet_using_apt() {
     wget --no-check-certificate https://packages.microsoft.com/config/ubuntu/22.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
     sudo dpkg -i packages-microsoft-prod.deb
     rm packages-microsoft-prod.deb
-
-    rm -rf /var/lib/apt/lists/*
-    check_packages apt-transport-https dotnet-sdk-6.0
+    
+    sudo apt-get update
+    sudo apt-get install -y apt-transport-https
+    sudo apt-get update
+    sudo apt-get install -y dotnet-sdk-6.0
 }
 
 # Install dependencies
