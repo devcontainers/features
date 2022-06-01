9705315f2681285d5b64103049c739eb1af1a713
JP Ungaretti <hello@jungaretti.com>
2022-06-01 21:37:01 +0000

    Restructure tools installation

--- src/python/install.sh
+++ src/python/install.sh
@@ -330,53 +330,54 @@ if [ "${PYTHON_VERSION}" != "none" ]; then
     updaterc "if [[ \"\${PATH}\" != *\"${CURRENT_PATH}/bin\"* ]]; then export PATH=${CURRENT_PATH}/bin:\${PATH}; fi"
 fi
 
-# If not installing python tools, exit
-if [ "${INSTALL_PYTHON_TOOLS}" != "true" ]; then
-    echo "Done!"
-    exit 0
-fi
+# Install Python tools if needed
+if [ "${INSTALL_PYTHON_TOOLS}" = 'true' ]; then
+    echo 'Installing Python tools...'
+    export PIPX_BIN_DIR="${PIPX_HOME}/bin"
+    export PATH="${CURRENT_PATH}/bin:${PIPX_BIN_DIR}:${PATH}"
 
-export PIPX_BIN_DIR="${PIPX_HOME}/bin"
-export PATH="${CURRENT_PATH}/bin:${PIPX_BIN_DIR}:${PATH}"
-
-# Create pipx group, dir, and set sticky bit
-if ! cat /etc/group | grep -e "^pipx:" > /dev/null 2>&1; then
-    groupadd -r pipx
-fi
-usermod -a -G pipx ${USERNAME}
-umask 0002
-mkdir -p ${PIPX_BIN_DIR}
-chown :pipx ${PIPX_HOME} ${PIPX_BIN_DIR}
-chmod g+s ${PIPX_HOME} ${PIPX_BIN_DIR}
-
-# Update pip if not using os provided python
-if [ ${PYTHON_VERSION} != "os-provided" ] && [ ${PYTHON_VERSION} != "system" ] && [ ${PYTHON_VERSION} != "none" ]; then
-    echo "Updating pip..."
-    ${INSTALL_PATH}/bin/python3 -m pip install --no-cache-dir --upgrade pip
-fi
-
-# Install tools
-echo "Installing Python tools..."
-export PYTHONUSERBASE=/tmp/pip-tmp
-export PIP_CACHE_DIR=/tmp/pip-tmp/cache
-pipx_path=""
-if ! type pipx > /dev/null 2>&1; then
-    pip3 install --disable-pip-version-check --no-cache-dir --user pipx 2>&1
-    /tmp/pip-tmp/bin/pipx install --pip-args=--no-cache-dir pipx
-    pipx_path="/tmp/pip-tmp/bin/"
-fi
-for util in ${DEFAULT_UTILS[@]}; do
-    if ! type ${util} > /dev/null 2>&1; then
-        ${pipx_path}pipx install --system-site-packages --pip-args '--no-cache-dir --force-reinstall' ${util}
-    else
-        echo "${util} already installed. Skipping."
+    # Create pipx group, dir, and set sticky bit
+    if ! cat /etc/group | grep -e "^pipx:" > /dev/null 2>&1; then
+        groupadd -r pipx
     fi
-done
-rm -rf /tmp/pip-tmp
+    usermod -a -G pipx ${USERNAME}
+    umask 0002
+    mkdir -p ${PIPX_BIN_DIR}
+    chown :pipx ${PIPX_HOME} ${PIPX_BIN_DIR}
+    chmod g+s ${PIPX_HOME} ${PIPX_BIN_DIR}
 
-updaterc "$(cat << EOF
+    # Update pip if not using os provided python
+    if [ ${PYTHON_VERSION} != "os-provided" ] && [ ${PYTHON_VERSION} != "system" ] && [ ${PYTHON_VERSION} != "none" ]; then
+        echo "Updating pip..."
+        ${INSTALL_PATH}/bin/python3 -m pip install --no-cache-dir --upgrade pip
+    fi
+
+    # Install tools
+    echo "Installing Python tools..."
+    export PYTHONUSERBASE=/tmp/pip-tmp
+    export PIP_CACHE_DIR=/tmp/pip-tmp/cache
+    pipx_path=""
+    if ! type pipx > /dev/null 2>&1; then
+        pip3 install --disable-pip-version-check --no-cache-dir --user pipx 2>&1
+        /tmp/pip-tmp/bin/pipx install --pip-args=--no-cache-dir pipx
+        pipx_path="/tmp/pip-tmp/bin/"
+    fi
+    for util in ${DEFAULT_UTILS[@]}; do
+        if ! type ${util} > /dev/null 2>&1; then
+            ${pipx_path}pipx install --system-site-packages --pip-args '--no-cache-dir --force-reinstall' ${util}
+        else
+            echo "${util} already installed. Skipping."
+        fi
+    done
+    rm -rf /tmp/pip-tmp
+
+    updaterc "$(cat << EOF
 export PIPX_HOME="${PIPX_HOME}"
 export PIPX_BIN_DIR="${PIPX_BIN_DIR}"
 if [[ "\${PATH}" != *"\${PIPX_BIN_DIR}"* ]]; then export PATH="\${PATH}:\${PIPX_BIN_DIR}"; fi
 EOF
 )"
+fi
+
+echo "Done!"
+exit 0
