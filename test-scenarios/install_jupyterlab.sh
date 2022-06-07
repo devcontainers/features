3c5e6c45e7c472d7a019d330225c922a2ecd1b35
JP Ungaretti <hello@jungaretti.com>
2022-06-07 23:11:04 +0000

    Update test names

--- test-scenarios/install_jupyterlab.sh
+++ test-scenarios/install_jupyterlab.sh
@@ -5,8 +5,8 @@ set -e
 # Optional: Import test library
 source dev-container-features-test-lib
 
-check "jupyterlab" jupyter lab --version
-check "jupyterlab-config" grep 'allow_origin' /home/vscode/.jupyter/jupyter_notebook_config.py
+check "version" jupyter lab --version
+check "config" grep 'allow_origin' /home/vscode/.jupyter/jupyter_notebook_config.py
 
 # Report result
 reportResults
