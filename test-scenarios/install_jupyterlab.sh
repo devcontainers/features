13180ab57226adccb2100db1812f968f63769dfb
JP Ungaretti <hello@jungaretti.com>
2022-06-07 22:02:24 +0000

    Fix user in JupyterLab scenario

--- test-scenarios/install_jupyterlab.sh
+++ test-scenarios/install_jupyterlab.sh
@@ -5,7 +5,7 @@ set -e
 # Optional: Import test library
 source dev-container-features-test-lib
 
-check "jupyterlab" su - vscode -c "jupyter lab --version"
+check "jupyterlab" jupyter lab --version
 check "jupyterlab-config" grep 'allow_origin' /home/vscode/.jupyter/jupyter_notebook_config.py
 
 # Report result
