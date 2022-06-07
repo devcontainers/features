35ede187a74f2dfc673c073b3a0aee01c6e4f23c
JP Ungaretti <hello@jungaretti.com>
2022-06-07 21:25:52 +0000

    Create new scenario for JupyterLab

--- test/python/test.sh
+++ test/python/test.sh
@@ -8,10 +8,5 @@ source dev-container-features-test-lib
 # Definition specific tests
 check "version" python --version
 
-# Tests for optional arguments
-# check "jupyterlab" jupyter lab --version
-# check "jupyterlab-config" grep 'allow_origin' /home/vscode/.jupyter/jupyter_notebook_config.py
-# check "ml-packages" /usr/local/python/current/bin/python -c 'import numpy'
-
 # Report result
 reportResults
