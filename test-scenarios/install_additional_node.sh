768d26834c2b0c66bd178b71dfabb9813be62007
Josh Spicer <joshspicer@github.com>
2022-06-29 23:24:48 +0000

    remove minor for node 18

--- test-scenarios/install_additional_node.sh
+++ test-scenarios/install_additional_node.sh
@@ -3,9 +3,10 @@ set -e
 # Optional: Import test library
 source dev-container-features-test-lib
 
-check "version_on_path"  node -v | grep 18.4.0
+# 'latest' is some version of node 18 for a while.
+check "version_on_path"  node -v | grep 18
 
-check "v18_installed" ls -1 /usr/local/share/nvm/versions/node | grep 18.4.0
+check "v18_installed" ls -1 /usr/local/share/nvm/versions/node | grep 18
 check "v14_installed" ls -1 /usr/local/share/nvm/versions/node | grep 14.19.3
 check "v17_installed" ls -1 /usr/local/share/nvm/versions/node | grep 17.9.1
 
