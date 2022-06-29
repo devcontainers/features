272e47de26e1da8a1ba520d1bc5c8fa9dc033b35
Josh Spicer <joshspicer@github.com>
2022-06-29 23:21:39 +0000

    update test scenario

--- test-scenarios/install_additional_node.sh
+++ test-scenarios/install_additional_node.sh
@@ -3,29 +3,11 @@ set -e
 # Optional: Import test library
 source dev-container-features-test-lib
 
-function expect () {
-    local expected="$1"
-    local actual="$2"
-    if [ "$expected" != "$actual" ]; then
-        echo "Expected: $expected"
-        echo "Actual: $actual"
-        exit 1
-    fi
-}
-
-function string_contains () {
-    local string="$1"
-    local substring="$2"
-    if [ "${string#*$substring}" != "$string" ]; then
-        return 0
-    else
-        return 1
-    fi
-}
- 
 check "version_on_path"  node -v | grep 18.4.0
-string_contains "v14_installed"    "v14.19.3"   "$(ls -1 /usr/local/share/nvm/versions/node)"
-check ls -1 /usr/local/share/nvm/versions/node | grep 17.9.1
+
+check "v18_installed" ls -1 /usr/local/share/nvm/versions/node | grep 18.4.0
+check "v14_installed" ls -1 /usr/local/share/nvm/versions/node | grep 14.19.3
+check "v17_installed" ls -1 /usr/local/share/nvm/versions/node | grep 17.9.1
 
 
 # Report result
