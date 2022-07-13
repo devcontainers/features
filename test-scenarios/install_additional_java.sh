96b02910c9a7965f466a640f67b046154ff21e0c
Samruddhi Khandale <skhandale@microsoft.com>
2022-07-13 14:02:45 -0700

    nit

--- test-scenarios/install_additional_java.sh
+++ test-scenarios/install_additional_java.sh
@@ -9,6 +9,5 @@ check "java version 17 installed as default" java --version | grep 17
 check "java version 11 installed"  ls /usr/local/sdkman/candidates/java  | grep 11
 check "java version 8 installed"  ls /usr/local/sdkman/candidates/java | grep 8
 
-
 # Report result
 reportResults
