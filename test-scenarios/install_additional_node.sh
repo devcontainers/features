01b46faa2dd7d9bd4f453b78f49457f2ffc67d3c
Josh Spicer <joshspicer@github.com>
2022-07-14 14:12:11 -0400

    smart auto-install multiple versions for `node` (#56)
    * node
    
    * fix comment (no-ci)
    
    * Apply suggestions from code review
    
    Co-authored-by: Samruddhi Khandale <skhandale@microsoft.com>
    
    * update test scenario
    
    * remove minor for node 18
    
    * update scenarios to object notation
    
    * add shabang
    
    * only set default node once
    
    Co-authored-by: Samruddhi Khandale <skhandale@microsoft.com>

---
+++ test-scenarios/install_additional_node.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+# Optional: Import test library
+source dev-container-features-test-lib
+
+# 'latest' is some version of node 18 for a while.
+check "version_on_path"  node -v | grep 18
+
+check "v18_installed" ls -1 /usr/local/share/nvm/versions/node | grep 18
+check "v14_installed" ls -1 /usr/local/share/nvm/versions/node | grep 14.19.3
+check "v17_installed" ls -1 /usr/local/share/nvm/versions/node | grep 17.9.1
+
+
+# Report result
+reportResults
