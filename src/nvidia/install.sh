82cf979c8a02acc888d4a3ca1b4bb6cfd697ffef
JP Ungaretti <hello@jungaretti.com>
2022-08-04 23:13:43 +0000

    Update comments

--- src/nvidia/install.sh
+++ src/nvidia/install.sh
@@ -6,32 +6,31 @@ install_cuda=${INSTALL_CUDA:-"true"}
 install_cudnn=${INSTALL_CUDNN:-"true"}
 
 if [ "$(id -u)" -ne 0 ]; then
-    echo "Must run as root"
+    echo -e 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
     exit 1
 fi
 
-keyring_package="cuda-keyring_1.0-1_all.deb"
+# Download Nvidia's keyring package
 ubuntu_version="$(lsb_release -sr | sed 's/\.//g')"
 keyring_repo="https://developer.download.nvidia.com/compute/cuda/repos/ubuntu$ubuntu_version/x86_64"
+keyring_package="cuda-keyring_1.0-1_all.deb"
 keyring_package_url="$keyring_repo/$keyring_package"
-
-# keyring_package="cuda-keyring_1.0-1_all.deb"
-# keyring_repo="https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64"
-# keyring_package_url="$keyring_repo/$keyring_package"
-
 keyring_package_path="$(mktemp -d)"
 keyring_package_file="$keyring_package_path/$keyring_package"
-
 wget -O "$keyring_package_file" "$keyring_package_url"
+
+# Install Nvidia's keyring package
 apt-get install -yq "$keyring_package_file"
 apt-get update -yq
 
 if [ "$install_cuda" = "true" ]; then
     echo "Installing CUDA libraries..."
     apt-get install -yq cuda-libraries-11-7
+    echo "Successfully installed CUDA libraries!"
 fi
 
 if [ "$install_cudnn" = "true" ]; then
     echo "Installing cuDNN libraries..."
     apt-get install -yq libcudnn8
+    echo "Successfully installed cuDNN libraries!"
 fi
