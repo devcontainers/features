4c412ff04d2d2d5022489a196a3baf550bfcee0b
Josh Spicer <joshspicer@github.com>
2022-08-11 19:58:26 +0000

    use default jammy apt feed to mitigate jammy/oryx install issues

--- test/dotnet/test.sh
+++ test/dotnet/test.sh
@@ -9,5 +9,7 @@ source dev-container-features-test-lib
 check "dotnet" dotnet --info
 check "sdks" dotnet --list-sdks
 
+check "some major version of dotnet 6 is installed" dotnet --list-sdks |  grep '6\.[0-9]*\.[0-9]*'
+
 # Report result
 reportResults
