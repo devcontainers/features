be09389ac637011ae4ffba3fcb818e775e9b3f7c
Josh Spicer <joshspicer@github.com>
2022-08-11 21:45:11 +0000

    better greps

--- test-scenarios/install_dotnet_5.sh
+++ test-scenarios/install_dotnet_5.sh
@@ -7,7 +7,7 @@ source dev-container-features-test-lib
 
 check "dotnet sdks" dotnet --list-sdks
 check "some major version of dotnet 5 is installed" dotnet --list-sdks |  grep '5\.[0-9]*\.[0-9]*'
-check "dotnet version 5 installed"  ls -l /usr/local/dotnet | grep 5
+check "dotnet version 5 installed"  ls -l /usr/local/dotnet | grep '5\.[0-9]*\.[0-9]*'
 
 
 # Report result
