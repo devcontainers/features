c03a16b0c8a0c0687bf3e187eada574fafb0a403
Josh Spicer <joshspicer@github.com>
2022-08-11 20:00:09 +0000

    remove old comment

--- src/oryx/install.sh
+++ src/oryx/install.sh
@@ -3,9 +3,7 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See https://go.microsoft.com/fwlink/?linkid=2090316 for license information.
 #-------------------------------------------------------------------------------------------------------------
-#
-# Docs: https://github.com/microsoft/vscode-dev-containers/blob/main/script-library/docs/hugo.md
-# Maintainer: The VS Code and Codespaces Teams
+
 
 USERNAME=${USERNAME:-"automatic"}
 UPDATE_RC=${UPDATE_RC:-"true"}
