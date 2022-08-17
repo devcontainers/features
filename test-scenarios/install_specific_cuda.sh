d3e145b2a78266a329e1772c9842b4ab3e9ed7f9
JP Ungaretti <hello@jungaretti.com>
2022-08-17 00:45:37 +0000

    Add scenario to test specific CUDA/cuDNN version

---
+++ test-scenarios/install_specific_cuda.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+# Optional: Import test library
+source dev-container-features-test-lib
+
+check "libcudnn.so.8" test 1 -eq "$(find /usr -name 'libcudnn.so.8.3.2' | wc -l)"
+check "nvtx" test -e '/usr/local/cuda-11.7/targets/x86_64-linux/include/nvtx3'
+
+# Report result
+reportResults
--- test/nvidia-cuda/test.sh
+++ test/nvidia-cuda/test.sh
@@ -14,9 +14,5 @@ check "libcurand.so.10" test 1 -eq "$(find /usr -name 'libcurand.so.10' | wc -l)
 check "libcusolver.so.11" test 1 -eq "$(find /usr -name 'libcusolver.so.11' | wc -l)"
 check "libcusparse.so.11" test 1 -eq "$(find /usr -name 'libcusparse.so.11' | wc -l)"
 
-# TODO: Move these to a new scenario for this feature
-# check "libcudnn.so.8" test 1 -eq "$(find /usr -name 'libcudnn.so.8' | wc -l)"
-# check "nvtx" test -e '/usr/local/cuda-11.7/targets/x86_64-linux/include/nvtx3'
-
 # Report result
 reportResults
