11d217abe9a88246455d28e7b43ca642072bab7e
JP Ungaretti <hello@jungaretti.com>
2022-08-18 00:24:47 +0000

    Add more helpful error messages

--- src/nvidia-cuda/install.sh
+++ src/nvidia-cuda/install.sh
@@ -10,6 +10,8 @@ CUDNN_VERSION=${CUDNNVERSION:-"latest"}
 # NVIDIA's package names include this information
 LATEST_CUDA_VERSION="11.7"
 LATEST_CUDNN_VERSION="8.5.0.96-1"
+if [ "$CUDA_VERSION" = "latest" ]; then CUDA_VERSION="$LATEST_CUDA_VERSION"; fi
+if [ "$CUDNN_VERSION" = "latest" ]; then CUDNN_VERSION="$LATEST_CUDNN_VERSION"; fi
 
 if [ "$(id -u)" -ne 0 ]; then
     echo -e 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
@@ -17,14 +19,23 @@ if [ "$(id -u)" -ne 0 ]; then
 fi
 
 # Function to run apt-get if needed
-apt_get_update_if_needed()
-{
+apt_get_update_if_needed() {
     if [ ! -d "/var/lib/apt/lists" ] || [ "$(ls /var/lib/apt/lists/ | wc -l)" = "0" ]; then
         echo "Running apt-get update..."
         apt-get update
     else
         echo "Skipping apt-get update."
     fi
+}
+
+# Prints a more helpful error message when installation fails
+apt_try_install() {
+    apt-get install -yq "$1" || {
+        local exit_code=$?
+        echo "Failed to install $1"
+        echo "See $NVIDIA_REPO_URL for all available packages and versions"
+        return $exit_code
+    }
 }
 
 # Checks if packages are installed and installs them if not
@@ -48,20 +59,17 @@ wget -O "$KEYRING_PACKAGE_FILE" "$KEYRING_PACKAGE_URL"
 apt-get install -yq "$KEYRING_PACKAGE_FILE"
 apt-get update -yq
 
-if [ "$CUDA_VERSION" = "latest" ]; then CUDA_VERSION="$LATEST_CUDA_VERSION"; fi
-if [ "$CUDNN_VERSION" = "latest" ]; then CUDNN_VERSION="$LATEST_CUDNN_VERSION"; fi
-
 echo "Installing CUDA libraries..."
-apt-get install -yq cuda-libraries-${CUDA_VERSION/./-}
+apt_try_install "cuda-libraries-${CUDA_VERSION/./-}"
 
 if [ "$INSTALL_CUDNN" = "true" ]; then
     echo "Installing cuDNN libraries..."
-    apt-get install -yq libcudnn8=${CUDNN_VERSION}+cuda${CUDA_VERSION}
+    apt_try_install "libcudnn8=${CUDNN_VERSION}+cuda${CUDA_VERSION}"
 fi
 
 if [ "$INSTALL_NVTX" = "true" ]; then
     echo "Installing NVTX..."
-    apt-get install -yq cuda-nvtx-${CUDA_VERSION/./-}
+    apt_try_install "cuda-nvtx-${CUDA_VERSION/./-}"
 fi
 
 echo "Done!"
