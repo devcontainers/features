fe2badaea143ede05da7e79e04f6156747fbb11b
JP Ungaretti <19893438+jungaretti@users.noreply.github.com>
2022-08-21 09:59:33 -0700

    Comments and feature description

--- test-scenarios/install_cudnn_nvxt.sh
+++ test-scenarios/install_cudnn_nvxt.sh
@@ -5,10 +5,10 @@ set -e
 # Optional: Import test library
 source dev-container-features-test-lib
 
-# Make sure that libcudnn8 installed correctly
+# Check installation of libcudnn8
 check "libcudnn.so.8" test 1 -eq "$(find /usr -name 'libcudnn.so.8' | wc -l)"
 
-# Make sure that cuda-nvtx-11-<minor version> installed correctly
+# Check installation of cuda-nvtx-11-<version>
 check "cuda-11+nvtx" test -e '/usr/local/cuda-11/targets/x86_64-linux/include/nvtx3'
 
 # Report result
--- test-scenarios/install_cudnn_nvxt_version.sh
+++ test-scenarios/install_cudnn_nvxt_version.sh
@@ -5,11 +5,12 @@ set -e
 # Optional: Import test library
 source dev-container-features-test-lib
 
-# Make sure that libcudnn8 (8.3.2) installed correctly
+# Check installation of libcudnn8 (8.3.2)
 check "libcudnn.so.8.3.2" test 1 -eq "$(find /usr -name 'libcudnn.so.8.3.2' | wc -l)"
 
-# Make sure that cuda-nvtx-11-5 (11.5) installed correctly
+# Check installation of cuda-nvtx-11-5 (11.5)
 check "cuda-11-5+nvtx" test -e '/usr/local/cuda-11.5/targets/x86_64-linux/include/nvtx3'
 
 # Report result
 reportResults
+    
