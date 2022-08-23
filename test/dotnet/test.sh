e45023c6e6c5558d35848e9a425ba4074becfabc
Josh Spicer <joshspicer@github.com>
2022-08-23 00:27:30 +0000

    update test

--- test/dotnet/test.sh
+++ test/dotnet/test.sh
@@ -8,9 +8,10 @@ source dev-container-features-test-lib
 # Definition specific tests
 check "dotnet" dotnet --info
 check "sdks" dotnet --list-sdks
+check "version" dotnet --version
 
 echo "Validating expected version present..."
-check "some major version of dotnet 6 is installed" dotnet --list-sdks |  grep '6\.[0-9]*\.[0-9]*'
+check "some major version of dotnet 6 is installed" dotnet --version |  grep '6\.[0-9]*\.[0-9]*'
 
 # Report result
 reportResults
