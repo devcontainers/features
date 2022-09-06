a9fd287287a85b8fc18e731ccee685ced001b5ba
Samruddhi Khandale <skhandale@microsoft.com>
2022-09-06 18:04:13 +0000

    move config

--- src/anaconda/install.sh
+++ src/anaconda/install.sh
@@ -103,4 +103,22 @@ if ! conda --version &> /dev/null ; then
     updaterc "export CONDA_DIR=${CONDA_DIR}/bin"
 fi
 
+# Display a notice on conda when not running in GitHub Codespaces
+cat << 'EOF' > /usr/local/etc/vscode-dev-containers/conda-notice.txt
+When using "conda" from outside of GitHub Codespaces, note the Anaconda repository contains
+restrictions on commercial use that may impact certain organizations. See https://aka.ms/ghcs-conda
+
+EOF
+
+notice_script="$(cat << 'EOF'
+if [ -t 1 ] && [ "${IGNORE_NOTICE}" != "true" ] && [ "${TERM_PROGRAM}" = "vscode" ] && [ "${CODESPACES}" != "true" ] && [ ! -f "$HOME/.config/vscode-dev-containers/conda-notice-already-displayed" ]; then
+    cat "/usr/local/etc/vscode-dev-containers/conda-notice.txt"
+    mkdir -p "$HOME/.config/vscode-dev-containers"
+    ((sleep 10s; touch "$HOME/.config/vscode-dev-containers/conda-notice-already-displayed") &)
+fi
+EOF
+)"
+
+echo "${notice_script}" | tee -a /etc/bash.bashrc >> /etc/zsh/zshrc
+
 echo "Done!"
--- test/conda/test.sh
+++ test/conda/test.sh
@@ -7,6 +7,7 @@ source dev-container-features-test-lib
 
 # Definition specific tests
 check "version" conda --version
+check "if conda-notice.txt exists" cat /usr/local/etc/vscode-dev-containers/conda-notice.txt
 
 # Report result
 reportResults
