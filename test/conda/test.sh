1681f99d1d2932a8d85f725815be110431c2470e
Samruddhi Khandale <skhandale@microsoft.com>
2022-09-13 19:37:41 +0000

    conda script

--- test/conda/test.sh
+++ test/conda/test.sh
@@ -9,5 +9,8 @@ source dev-container-features-test-lib
 check "version" conda --version
 check "if conda-notice.txt exists" cat /usr/local/etc/vscode-dev-containers/conda-notice.txt
 
+# Check env
+check "CONDA_SCRIPT is set correctly" echo $CONDA_SCRIPT | grep "/opt/conda/etc/profile.d/conda.sh"
+
 # Report result
 reportResults
