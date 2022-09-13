15d152c80d2da95759965e61dad47118b82ca981
Samruddhi Khandale <skhandale@microsoft.com>
2022-09-13 19:56:11 +0000

    modify tests

--- test/java/install_gradle_and_maven_for_user.sh
+++ test/java/install_gradle_and_maven_for_user.sh
@@ -5,15 +5,17 @@ set -e
 # Optional: Import test library
 source dev-container-features-test-lib
 
+check "user is vscode" whoami | grep vscode
+
 check "java" java --version
 check "gradle" gradle --version
 
 cd /tmp && gradle init --type basic --dsl groovy --incubating --project-name test
-check "GRADLE_USER_HOME exists" ls -la ~ | grep .gradle
+check "GRADLE_USER_HOME exists" ls -la /home/vscode | grep ".gradle"
 
 check "maven" mvn --version
 cd /tmp && mvn archetype:generate -DgroupId=com.mycompany.app -DartifactId=my-app -DarchetypeArtifactId=maven-archetype-quickstart -DarchetypeVersion=1.4 -DinteractiveMode=false
-check "m2 exists" ls -la ~ | grep .m2
+check "m2 exists" ls -la /home/vscode | grep ".m2"
 
 # Report result
 reportResults
