10d849a0ff0b6760364370e224b2ced7555a2558
Samruddhi Khandale <skhandale@microsoft.com>
2022-09-19 21:37:34 +0000

    add jdkDistro option

--- test/java/install_from_non_default_distro.sh
+++ test/java/install_from_non_default_distro.sh
@@ -6,8 +6,6 @@ set -e
 source dev-container-features-test-lib
 
 check "java version 18 installed as default" java --version | grep 18
-check "java version 11 installed"  ls /usr/local/sdkman/candidates/java  | grep 11
-check "java version 8 installed"  ls /usr/local/sdkman/candidates/java | grep 8
 
 # Report result
 reportResults
