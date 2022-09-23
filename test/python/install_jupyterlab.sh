ceefff98ee419a99acdadabefee55ec5d8d068e4
JP Ungaretti <hello@jungaretti.com>
2022-09-23 22:57:28 +0000

    Remove bloat from JupyterLab test

--- test/python/install_jupyterlab.sh
+++ test/python/install_jupyterlab.sh
@@ -5,12 +5,17 @@ set -e
 # Optional: Import test library
 source dev-container-features-test-lib
 
-check "version" jupyter lab --version
-check "config" grep ".*.allow_origin = '*'" /home/vscode/.jupyter/jupyter_server_config.py
-
+# Run these checks as the non-root user
 check "user" whoami | grep vscode
-check "zsh" zsh --version
-check "wget" wget -V
+
+# Check for an installation of JupyterLab
+check "version" jupyter lab --version
+
+# Check location of JupyterLab installation
+# check "location" /usr/local/python/current/bin/python3 -m pip list | grep jupyter
+
+# Check for correct JupyterLab configuration
+check "config" grep ".*.allow_origin = '*'" /home/vscode/.jupyter/jupyter_server_config.py
 
 # Report result
 reportResults
