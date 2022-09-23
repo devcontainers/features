861e65c68dd6536f43e429ae2d3883a1c706d495
JP Ungaretti <hello@jungaretti.com>
2022-09-23 23:13:09 +0000

    Check location of JupyterLab installation

--- test/python/install_jupyterlab.sh
+++ test/python/install_jupyterlab.sh
@@ -5,14 +5,16 @@ set -e
 # Optional: Import test library
 source dev-container-features-test-lib
 
-# Run these checks as the non-root user
-check "user" whoami | grep vscode
+# Always run these checks as the non-root user
+user="$(whoami)"
+check "user" grep vscode <<< "$user"
 
 # Check for an installation of JupyterLab
 check "version" jupyter lab --version
 
 # Check location of JupyterLab installation
-# check "location" /usr/local/python/current/bin/python3 -m pip list | grep jupyter
+packages="$(python3 -m pip list)"
+check "location" grep jupyter <<< "$packages"
 
 # Check for correct JupyterLab configuration
 check "config" grep ".*.allow_origin = '*'" /home/vscode/.jupyter/jupyter_server_config.py
