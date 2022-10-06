4f48ac2166858cd569cf49d08d1add23803f5e8a
Samruddhi Khandale <skhandale@microsoft.com>
2022-10-06 17:44:09 +0000

    add DEV_CONTAINERS_DIR as containerEnv

--- test/common-utils/test.sh
+++ test/common-utils/test.sh
@@ -8,6 +8,7 @@ source dev-container-features-test-lib
 # Definition specific tests
 check "jq" jq  --version
 check "curl" curl  --version
+check "DEV_CONTAINERS_DIR is set correctly" echo $DEV_CONTAINERS_DIR | grep "/usr/local/etc/vscode-dev-containers"
 
 # Report result
 reportResults
