b0db2671ebf7c154a216a791870179bcf7b0f34b
Samruddhi Khandale <skhandale@microsoft.com>
2022-10-18 17:59:22 +0000

    add another check

--- test/common-utils/test.sh
+++ test/common-utils/test.sh
@@ -8,6 +8,7 @@ source dev-container-features-test-lib
 # Definition specific tests
 check "jq" jq  --version
 check "curl" curl  --version
+check "git" git --version
 
 git_version_satisfied=false
 if (echo a version 2.38.1; git --version) | sort -Vk3 | tail -1 | grep -q git; then
