4d915904488c61c816e3626a53935fe959c26a3f
Josh Spicer <josh@joshspicer.com>
2022-10-20 18:25:15 +0000

    update test, add stub for dockerDefaultAddressPool

--- test/docker-in-docker/dockerDefaultAddressPool.sh
+++ test/docker-in-docker/dockerDefaultAddressPool.sh
@@ -8,9 +8,12 @@ source dev-container-features-test-lib
 # Definition specific tests
 check "version" docker  --version
 
-check "ps" docker ps
+check "ps" docker  ps
 
 check "run hello world" docker run hello-world
+
+# Asserts specific to manually setting dockerDefaultAddressPool
+# TODO
 
 # Report result
 reportResults
