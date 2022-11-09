ac9f750ac791b87996c1c613b5000bd310c15b83
etienne-chrp <66628513+etienne-chrp@users.noreply.github.com>
2022-11-09 09:14:04 +0000

    Fix java feature tests about java version
    Java 18 is not available anymore in SDKMAN
    
    Some tests asserts were generating false positives

--- test/java/install_additional_java.sh
+++ test/java/install_additional_java.sh
@@ -5,9 +5,9 @@ set -e
 # Optional: Import test library
 source dev-container-features-test-lib
 
-check "java version 18 installed as default" grep "18" <(java --version)
-check "java version 11 installed" grep "11" <(ls /usr/local/sdkman/candidates/java)
-check "java version 8 installed" grep "8" <(ls /usr/local/sdkman/candidates/java)
+check "java version 11 installed as default" grep "11\." <(java --version)
+check "java version 17 installed" grep "^17\." <(ls /usr/local/sdkman/candidates/java)
+check "java version 8 installed" grep "^8\." <(ls /usr/local/sdkman/candidates/java)
 
 # Report result
 reportResults
--- test/java/install_from_non_default_distro.sh
+++ test/java/install_from_non_default_distro.sh
@@ -5,7 +5,7 @@ set -e
 # Optional: Import test library
 source dev-container-features-test-lib
 
-check "java version 18 installed" grep "18" <(java --version)
+check "java version openjdk 11 installed" grep "openjdk 11." <(java --version)
 
 # Report result
 reportResults
