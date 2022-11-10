9a1b8a7b30436091e2f1c43e2287e4f3af0f7ee5
Chuck Lantz <chuck_lantz@hotmail.com>
2022-11-10 22:39:45 +0000

    Fix, improve tests

--- test/dotnet/install_wo_apt.sh
+++ test/dotnet/install_wo_apt.sh
@@ -6,8 +6,8 @@ set -e
 source dev-container-features-test-lib
 
 check "dotnet sdks" dotnet --list-sdks
-check "some major version of dotnet 3 is installed" bash -c "dotnet --list-sdks |  grep '3\.[0-9]*\.[0-9]*'"
-check "dotnet version 3 installed"  bash -c "ls -l /usr/share/dotnet/sdk | grep '3\.[0-9]*\.[0-9]*'"
+check "some major version of dotnet 7 is installed" bash -c "dotnet --list-sdks |  grep '7\.[0-9]*\.[0-9]*'"
+check "dotnet version 7 installed"  bash -c "ls -l /usr/local/dotnet | grep '7\.[0-9]*\.[0-9]*'"
 
 # Verify current symlink exists and works
 check "current link dotnet" /usr/local/dotnet/current/dotnet --info
