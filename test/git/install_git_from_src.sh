0489034c35266c49c236efe961bdff6dd534a10a
Samruddhi Khandale <skhandale@microsoft.com>
2022-11-10 21:27:07 +0000

    add gitconfig test

--- test/git/install_git_from_src.sh
+++ test/git/install_git_from_src.sh
@@ -6,6 +6,7 @@ set -e
 source dev-container-features-test-lib
 
 check "source-dir" bash -c "which git | grep /usr/bin/git"
+check "gitconfig-location" bash -c "git config --system user.name devcontainer && ls /etc | grep gitconfig"
 check "version" git  --version
 check "gettext" dpkg-query -l gettext
 
