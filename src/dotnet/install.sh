614641c40a30009a47ec8c763de5336a79ad1c09
Chuck Lantz <chuck_lantz@hotmail.com>
2022-11-14 19:27:36 +0000

    Remove unused function

--- src/dotnet/install.sh
+++ src/dotnet/install.sh
@@ -87,21 +87,6 @@ cleanup() {
 }
 trap cleanup EXIT
 
-# Get central common setting
-get_common_setting() {
-    if [ "${common_settings_file_loaded}" != "true" ]; then
-        curl -sfL "https://aka.ms/vscode-dev-containers/script-library/settings.env" 2>/dev/null -o /tmp/vsdc-settings.env || echo "Could not download settings file. Skipping."
-        common_settings_file_loaded=true
-    fi
-    if [ -f "/tmp/vsdc-settings.env" ]; then
-        local multi_line=""
-        if [ "$2" = "true" ]; then multi_line="-z"; fi
-        local result="$(grep ${multi_line} -oP "$1=\"?\K[^\"]+" /tmp/vsdc-settings.env | tr -d '\0')"
-        if [ ! -z "${result}" ]; then declare -g $1="${result}"; fi
-    fi
-    echo "$1=${!1}"
-}
-
 # Add dotnet directory to PATH in bashrc/zshrc files if OVERRIDE_DEFAULT_VERSION=true.
 updaterc() {
     if [ "${UPDATE_RC}" = "true" ]; then
