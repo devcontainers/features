be4f0498e4ad7948804d4be70093c1da8722d222
etienne-chrp <66628513+etienne-chrp@users.noreply.github.com>
2022-11-17 01:46:30 +0100

    Add Gradle and Maven version parameters to Java (#263)
    * Refactor java feature tests grep syntax
    
    The grep with pipe syntax was creating problems with the check helper
    
    Change them in favour of process substitution syntax
    
    * Fix java feature tests about java version
    
    Java 18 is not available anymore in SDKMAN
    
    Some tests asserts were generating false positives
    
    * Add Gradle and Maven version parameters to Java

--- test/java/install_gradle_and_maven_for_user.sh
+++ test/java/install_gradle_and_maven_for_user.sh
@@ -5,17 +5,17 @@ set -e
 # Optional: Import test library
 source dev-container-features-test-lib
 
-check "user is vscode" whoami | grep vscode
+check "user is vscode" grep vscode <(whoami)
 
 check "java" java --version
 check "gradle" gradle --version
 
 cd /tmp && gradle init --type basic --dsl groovy --incubating --project-name test
-check "GRADLE_USER_HOME exists" ls -la /home/vscode | grep ".gradle"
+check "GRADLE_USER_HOME exists" grep ".gradle" <(ls -la /home/vscode)
 
 check "maven" mvn --version
 cd /tmp && mvn archetype:generate -DgroupId=com.mycompany.app -DartifactId=my-app -DarchetypeArtifactId=maven-archetype-quickstart -DarchetypeVersion=1.4 -DinteractiveMode=false
-check "m2 exists" ls -la /home/vscode | grep ".m2"
+check "m2 exists" grep ".m2" <(ls -la /home/vscode)
 
 # Report result
 reportResults
