18db2f09c7052a06625b043209a8d8a8cae154a4
Josh Spicer <joshspicer@github.com>
2022-11-18 00:25:06 +0000

    attempt to debug why this is not working in Action

--- test/dotnet/install_non_root_remoteUser.sh
+++ test/dotnet/install_non_root_remoteUser.sh
@@ -7,6 +7,18 @@ source dev-container-features-test-lib
 
 check "ensure i am user codespace"  bash -c "whoami | grep 'codespace'"
 
+echo "Echoing contents of '/home/codespace/' ...."
+ls -la /home/codespace/
+
+echo "Echoing contents of '/home/codespace/.dotnet' ...."
+ls -la /home/codespace/.dotnet/
+
+check  "symlinked '/home/codespace/.dotnet' folder has the correct permissions"  bash -c "ls -la /home/codespace | grep -E 'lrwxrwxrwx 1 codespace codespace  (.*) .dotnet -> /usr/local/dotnet/current'"
+
+
+
+check "A file following the symlink has the correct permissions" bash -c ""
+
 ./install_dotnet_7_jammy.sh
 
 # Report result
