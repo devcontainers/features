e7ebc0e9de1e3cf61424096352561d94b7e3e9a0
Mislav Marohnić <git@mislav.net>
2022-12-01 16:52:34 +0100

    github-cli: simplify logic for detecting available gh releases
    This change relies on `git ls-remote --tags --sort=version:refname` to sort git versions in a semver-compatible mode and alleviate the need to scan for version numbers manually. The new approach selects at most two latest releases that match VERSION and installs the first `.deb` that was successfully downloaded from a GitHub CLI release.
    
    This also removes support for installing a specific version number with `apt-get install gh=VERSION`, since the GitHub CLI package repository does not retain older gh versions and only allows installing the latest one.
    
    Finally, this removes version-selecting tests that only tested an implementation that was entirely contained within tests.
    
    Bonuses:
    - Only install curl, apt-transport-https, dirmngr, gnupg2 when needed.
    - Ensure `apt-get update` runs at most once.
    - Remove the side-effect of `/var/lib/apt/lists` being wiped out when the github-cli feature is activated.
    - Add ability to export bash functions for testing: `TEST_EXPORT=find_version_from_git_tags ./src/github-cli/install.sh`
    - Errors are printed to stderr.

--- src/github-cli/install.sh
+++ src/github-cli/install.sh
@@ -9,6 +9,7 @@
 
 CLI_VERSION=${VERSION:-"latest"}
 INSTALL_DIRECTLY_FROM_GITHUB_RELEASE=${INSTALLDIRECTLYFROMGITHUBRELEASE:-"true"}
+TEST_EXPORT=${TEST_EXPORT:-}
 
 GITHUB_CLI_ARCHIVE_GPG_KEY=23F3D4EA75716059
 GPG_KEY_SERVERS="keyserver hkp://keyserver.ubuntu.com:80
@@ -17,11 +18,8 @@ keyserver hkp://keyserver.pgp.com"
 
 set -e
 
-# Clean up
-rm -rf /var/lib/apt/lists/*
-
-if [ "$(id -u)" -ne 0 ]; then
-    echo -e 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
+if [ "$(id -u)" -ne 0 ] && [ -z "$TEST_EXPORT" ]; then
+    echo 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.' >&2
     exit 1
 fi
 
@@ -108,12 +106,14 @@ receive_gpg_keys() {
     fi
 }
 
-apt_get_update()
-{
-    if [ "$(find /var/lib/apt/lists/* | wc -l)" = "0" ]; then
-        echo "Running apt-get update..."
-        apt-get update -y
-    fi
+ran_apt_get_update="false"
+
+# Runs `apt-get update` at most once
+apt_get_update() {
+    [ "$ran_apt_get_update" != "true" ] || return 0
+    echo "Running apt-get update..."
+    apt-get update -y
+    ran_apt_get_update="true"
 }
 
 # Checks if packages are installed and installs them if not
@@ -124,138 +124,85 @@ check_packages() {
     fi
 }
 
-# Figure out correct version of a three part version number is not passed
+# Print latest git versions that match an optional version prefix
 find_version_from_git_tags() {
-    local variable_name=$1
-    local requested_version=${!variable_name}
-    if [ "${requested_version}" = "none" ]; then return; fi
-    local repository=$2
-    local prefix=${3:-"tags/v"}
-    local separator=${4:-"."}
-    local last_part_optional=${5:-"false"}    
-    if [ "$(echo "${requested_version}" | grep -o "." | wc -l)" != "2" ]; then
-        local escaped_separator=${separator//./\\.}
-        local last_part
-        if [ "${last_part_optional}" = "true" ]; then
-            last_part="(${escaped_separator}[0-9]+)?"
-        else
-            last_part="${escaped_separator}[0-9]+"
+    local prefix="${1:-}"
+    git ls-remote --tags --sort=-version:refname https://github.com/cli/cli.git | while read -r _ ref; do
+        version="${ref#refs/tags/v}"
+        if [[ $version != *-* && ( -z $prefix || "${version}." == "${prefix}."* ) ]]; then
+            printf "%s\n" "$version"
         fi
-        local regex="${prefix}\\K[0-9]+${escaped_separator}[0-9]+${last_part}$"
-        local version_list="$(git ls-remote --tags ${repository} | grep -oP "${regex}" | tr -d ' ' | tr "${separator}" "." | sort -rV)"
-        if [ "${requested_version}" = "latest" ] || [ "${requested_version}" = "current" ] || [ "${requested_version}" = "lts" ]; then
-            declare -g ${variable_name}="$(echo "${version_list}" | head -n 1)"
-        else
-            set +e
-            declare -g ${variable_name}="$(echo "${version_list}" | grep -E -m 1 "^${requested_version//./\\.}([\\.\\s]|$)")"
-            set -e
-        fi
-    fi
-    if [ -z "${!variable_name}" ] || ! echo "${version_list}" | grep "^${!variable_name//./\\.}$" > /dev/null 2>&1; then
-        echo -e "Invalid ${variable_name} value: ${requested_version}\nValid values:\n${version_list}" >&2
-        exit 1
-    fi
-    echo "${variable_name}=${!variable_name}"
+    done
 }
 
-# Use semver logic to decrement a version number then look for the closest match
-find_prev_version_from_git_tags() {
-    local variable_name=$1
-    local current_version=${!variable_name}
-    local repository=$2
-    # Normally a "v" is used before the version number, but support alternate cases
-    local prefix=${3:-"tags/v"}
-    # Some repositories use "_" instead of "." for version number part separation, support that
-    local separator=${4:-"."}
-    # Some tools release versions that omit the last digit (e.g. go)
-    local last_part_optional=${5:-"false"}
-    # Some repositories may have tags that include a suffix (e.g. actions/node-versions)
-    local version_suffix_regex=$6
-    # Try one break fix version number less if we get a failure. Use "set +e" since "set -e" can cause failures in valid scenarios.
-    set +e
-        major="$(echo "${current_version}" | grep -oE '^[0-9]+' || echo '')"
-        minor="$(echo "${current_version}" | grep -oP '^[0-9]+\.\K[0-9]+' || echo '')"
-        breakfix="$(echo "${current_version}" | grep -oP '^[0-9]+\.[0-9]+\.\K[0-9]+' 2>/dev/null || echo '')"
-
-        if [ "${minor}" = "0" ] && [ "${breakfix}" = "0" ]; then
-            ((major=major-1))
-            declare -g ${variable_name}="${major}"
-            # Look for latest version from previous major release
-            find_version_from_git_tags "${variable_name}" "${repository}" "${prefix}" "${separator}" "${last_part_optional}"
-        # Handle situations like Go's odd version pattern where "0" releases omit the last part
-        elif [ "${breakfix}" = "" ] || [ "${breakfix}" = "0" ]; then
-            ((minor=minor-1))
-            declare -g ${variable_name}="${major}.${minor}"
-            # Look for latest version from previous minor release
-            find_version_from_git_tags "${variable_name}" "${repository}" "${prefix}" "${separator}" "${last_part_optional}"
-        else
-            ((breakfix=breakfix-1))
-            if [ "${breakfix}" = "0" ] && [ "${last_part_optional}" = "true" ]; then
-                declare -g ${variable_name}="${major}.${minor}"
-            else 
-                declare -g ${variable_name}="${major}.${minor}.${breakfix}"
-            fi
-        fi
-    set -e
-}
-
-# Fall back on direct download if no apt package exists
-# Fetches .deb file to be installed with dpkg
+# For each version, fetch a .deb file from releases and install the first available one with dpkg
 install_deb_using_github() {
-    check_packages wget
-    arch=$(dpkg --print-architecture)
-
-    find_version_from_git_tags CLI_VERSION https://github.com/cli/cli
-    cli_filename="gh_${CLI_VERSION}_linux_${arch}.deb"
-
-    mkdir -p /tmp/ghcli
-    pushd /tmp/ghcli
-    wget https://github.com/cli/cli/releases/download/v${CLI_VERSION}/${cli_filename}
-    exit_code=$?
-    set -e
-    if [ "$exit_code" != "0" ]; then
-        # Handle situation where git tags are ahead of what was is available to actually download
-        echo "(!) github-cli version ${CLI_VERSION} failed to download. Attempting to fall back one version to retry..."
-        find_prev_version_from_git_tags CLI_VERSION https://github.com/cli/cli
-        wget https://github.com/cli/cli/releases/download/v${CLI_VERSION}/${cli_filename}
-    fi
-
-    dpkg -i /tmp/ghcli/${cli_filename}
-    popd
+    local arch="$(dpkg --print-architecture)"
+    local version download_url
+    for version; do
+        download_url="https://github.com/cli/cli/releases/download/v${version}/gh_${version}_linux_${arch}.deb"
+        wget -P /tmp/ghcli "$download_url" || continue
+        dpkg -i "/tmp/ghcli/$(basename "$download_url")"
+        break
+    done
     rm -rf /tmp/ghcli
 }
 
-export DEBIAN_FRONTEND=noninteractive
-
-# Install curl, apt-transport-https, curl, gpg, or dirmngr, git if missing
-check_packages curl ca-certificates apt-transport-https dirmngr gnupg2
-if ! type git > /dev/null 2>&1; then
-    check_packages git
+if [ -n "$TEST_EXPORT" ]; then
+    # print function named by TEST_EXPORT and exit
+    declare -f "$TEST_EXPORT"
+    exit 0
 fi
 
-# Soft version matching
-if [ "${CLI_VERSION}" != "latest" ] && [ "${CLI_VERSION}" != "lts" ] && [ "${CLI_VERSION}" != "stable" ]; then
-    find_version_from_git_tags CLI_VERSION "https://github.com/cli/cli"
-    version_suffix="=${CLI_VERSION}"
-else
-    version_suffix=""
+check_packages ca-certificates
+type -P git >/dev/null || check_packages git
+
+is_latest="false"
+
+case "$CLI_VERSION" in
+none )
+    # opt out of installing gh
+    exit 0
+    ;;
+latest | lts | stable | current )
+    # find two latest versions
+    versions=( $(find_version_from_git_tags | head -n2) )
+    is_latest="true"
+    ;;
+*.*.* )
+    # the exact version is specified explicitly
+    versions=( "$CLI_VERSION" )
+    ;;
+* )
+    # find two latest versions matching version prefix
+    versions=( $(find_version_from_git_tags "$CLI_VERSION" | head -n2) )
+    ;;
+esac
+
+if [ "${#versions[@]}" -eq 0 ]; then
+    printf 'error: no git tags in the cli/cli repository matched version "%s"\n' "$CLI_VERSION" >&2
+    exit 1
 fi
 
 # Install the GitHub CLI
 echo "Downloading github CLI..."
 
 if [ "${INSTALL_DIRECTLY_FROM_GITHUB_RELEASE}" = "true" ]; then
-    install_deb_using_github
+    check_packages wget
+    install_deb_using_github "${versions[@]}"
+elif [ "$is_latest" != "true" ]; then
+    # In its current implementation, the https://cli.github.com/packages repository does not retain older versions of gh,
+    # so we cannot allow installing arbitrary versions with the `apt-get install gh=VERSION` syntax.
+    printf 'error: cannot install non-latest version when "installDirectlyFromGitHubRelease" is disabled\n' >&2
+    exit 1
 else
-   # Import key safely (new method rather than deprecated apt-key approach) and install
+    check_packages curl apt-transport-https dirmngr gnupg2
+    # Import key safely (new method rather than deprecated apt-key approach) and install
     . /etc/os-release
     receive_gpg_keys GITHUB_CLI_ARCHIVE_GPG_KEY /usr/share/keyrings/githubcli-archive-keyring.gpg
     echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" > /etc/apt/sources.list.d/github-cli.list
     apt-get update
-    apt-get -y install "gh${version_suffix}"
+    apt-get -y install gh
     rm -rf "/tmp/gh/gnupg"
     echo "Done!"
 fi
-
-# Clean up
-rm -rf /var/lib/apt/lists/*
