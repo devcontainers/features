7fa90110d762797cc0b1c2fe8fcc028c9b813d56
Samruddhi Khandale <skhandale@microsoft.com>
2022-12-13 14:09:16 -0800

    Git: Build from source with USE_LIBPCRE (#331)

--- test/git/install_git_from_src.sh
+++ test/git/install_git_from_src.sh
@@ -8,5 +8,9 @@ source dev-container-features-test-lib
 check "version" git  --version
 check "gettext" dpkg-query -l gettext
 
+cd /tmp && git clone https://github.com/devcontainers/feature-starter.git
+cd feature-starter
+check "perl" bash -c "git -c grep.patternType=perl grep -q 'a.+b'"
+
 # Report result
 reportResults
