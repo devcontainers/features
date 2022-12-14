8e738559bb82ef9eca587bd9bd88435a1300aad3
Chuck Lantz <chuck_lantz@hotmail.com>
2022-12-14 03:49:48 +0000

    Fix location of first run marker

--- src/common-utils/scripts/rc_snippet.sh
+++ src/common-utils/scripts/rc_snippet.sh
@@ -9,7 +9,7 @@ if [ -t 1 ] && [[ "${TERM_PROGRAM}" = "vscode" || "${TERM_PROGRAM}" = "codespace
     elif [ -f "/workspaces/.codespaces/shared/first-run-notice.txt" ]; then
         cat "/workspaces/.codespaces/shared/first-run-notice.txt"
     fi
-    mkdir -p "$HOME/.config/dev-containers"
+    mkdir -p "$HOME/.config/vscode-dev-containers"
     # Mark first run notice as displayed after 10s to avoid problems with fast terminal refreshes hiding it
     ((sleep 10s; touch "$HOME/.config/vscode-dev-containers/first-run-notice-already-displayed") &)
 fi
