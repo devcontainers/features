d5d8a67c622abc339c347c891f1d7f5d4dfabf20
Samruddhi Khandale <skhandale@microsoft.com>
2023-01-06 22:10:00 +0000

    Conda: Update 'certifi' due to CVE-2022-23491

--- test/conda/test.sh
+++ test/conda/test.sh
@@ -12,5 +12,25 @@ check "if conda-notice.txt exists" cat /usr/local/etc/vscode-dev-containers/cond
 # Check env
 check "CONDA_SCRIPT is set correctly" echo $CONDA_SCRIPT | grep "/opt/conda/etc/profile.d/conda.sh"
 
+check-version-ge() {
+    LABEL=$1
+    CURRENT_VERSION=$2
+    REQUIRED_VERSION=$3
+    shift
+    echo -e "\n🧪 Testing $LABEL: '$CURRENT_VERSION' is >= '$REQUIRED_VERSION'"
+    local GREATER_VERSION=$((echo ${CURRENT_VERSION}; echo ${REQUIRED_VERSION}) | sort -V | tail -1)
+    if [ "${CURRENT_VERSION}" == "${GREATER_VERSION}" ]; then
+        echo "✅  Passed!"
+        return 0
+    else
+        echoStderr "❌ $LABEL check failed."
+        FAILED+=("$LABEL")
+        return 1
+    fi
+}
+
+certifiVersion=$(python -c "import certifi; print(certifi.__version__)")
+check-version-ge "certifi" "${certifiVersion}" "2022.12.07"
+
 # Report result
 reportResults
