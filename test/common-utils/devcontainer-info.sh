41e9d45f34cfcf9be7b638c103d92c0b01064e22
eitsupi <ts1s1andn@gmail.com>
2023-01-09 19:47:09 +0900

    more tests

--- test/common-utils/devcontainer-info.sh
+++ test/common-utils/devcontainer-info.sh
@@ -6,7 +6,14 @@ set -e
 source dev-container-features-test-lib
 
 # Definition specific tests
-check "devcontainer-info" sh -c "devcontainer-info | grep test-build"
+check "devcontainer-info version" sh -c "devcontainer-info | grep test-version"
+check "devcontainer-info id" sh -c "devcontainer-info | grep test-build"
+check "devcontainer-info variant" sh -c "devcontainer-info | grep test-variant"
+check "devcontainer-info repository" sh -c "devcontainer-info | grep test-repository"
+check "devcontainer-info release" sh -c "devcontainer-info | grep test-release"
+check "devcontainer-info revigion" sh -c "devcontainer-info | grep test-revision"
+check "devcontainer-info timestamp" sh -c "devcontainer-info | grep test-time"
+check "devcontainer-info url" sh -c "devcontainer-info | grep test-url"
 
 # Report result
 reportResults
