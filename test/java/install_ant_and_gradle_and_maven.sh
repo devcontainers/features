c68295822fa21e6ae82a2a5e620e6e65ac68cc37
Tobias Gesellchen <tobias@gesellix.de>
2023-01-18 21:09:35 +0100

    Add tests for Java/Ant

--- test/java/install_ant_and_gradle_and_maven.sh
+++ test/java/install_ant_and_gradle_and_maven.sh
@@ -8,8 +8,15 @@ source dev-container-features-test-lib
 check "user is root" grep root <(whoami)
 
 check "java" java --version
-check "gradle" gradle --version
 
+check "ant" ant -version
+cat << EOF > /tmp/build.xml
+<project><target name="init"><mkdir dir="ant-src"/></target></project>
+EOF
+cd /tmp && ant init
+check "ant-src exists" grep "ant-src" <(ls -la /tmp)
+
+check "gradle" gradle --version
 cd /tmp && gradle init --type basic --dsl groovy --incubating --project-name test
 check "GRADLE_USER_HOME exists" grep ".gradle" <(ls -la /root)
 
