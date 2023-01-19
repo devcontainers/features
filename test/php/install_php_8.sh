e94a636b6296f2f5dfc3d09ac0fccf341476332b
Samruddhi Khandale <skhandale@microsoft.com>
2023-01-19 14:46:16 -0800

    update checks

--- test/php/install_php_8.sh
+++ test/php/install_php_8.sh
@@ -5,7 +5,7 @@ set -e
 # Optional: Import test library
 source dev-container-features-test-lib
 
-check "php-version-8-is-installed" php --version | grep '8.'
+check "php-version-8-is-installed" bash -c "php --version | grep '8.'"
 
 # Report result
 reportResults
