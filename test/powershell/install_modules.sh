ef6da45bb4c0beab7b8e14821d203cc5bf81a6f9
ross-p-smith <rosmith@microsoft.com>
2023-01-28 22:54:50 +0000

    Add -Command to test

--- test/powershell/install_modules.sh
+++ test/powershell/install_modules.sh
@@ -6,8 +6,8 @@ set -e
 source dev-container-features-test-lib
 
 # Extension-specific tests
-check "az.resources" pwsh Get-Module -name az.resource
-check "az.storage" pwsh Get-Module -name az.storage
+check "az.resources" pwsh -Command "Get-Module -name az.resource"
+check "az.storage" pwsh -Command "Get-Module -name az.storage"
 
 # Report result
 reportResults
