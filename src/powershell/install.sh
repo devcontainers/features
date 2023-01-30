fa34f84898293de48ba38f647a38f54daf322f23
Ross Smith <ross-p-smith@users.noreply.github.com>
2023-01-30 17:32:39 +0000

    Update src/powershell/install.sh
    Co-authored-by: Samruddhi Khandale <skhandale@microsoft.com>

--- src/powershell/install.sh
+++ src/powershell/install.sh
@@ -13,7 +13,7 @@ set -e
 rm -rf /var/lib/apt/lists/*
 
 POWERSHELL_VERSION=${VERSION:-"latest"}
-POWERSHELL_MODULES=${MODULES}
+POWERSHELL_MODULES="${MODULES}"
 POWERSHELL_INSTALLBICEP=${INSTALLBICEP:-false}
 
 MICROSOFT_GPG_KEYS_URI="https://packages.microsoft.com/keys/microsoft.asc"
