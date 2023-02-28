d664137814a5f59a954df2b7a5fda665b4d50e97
JP Ungaretti <git@jungaretti.com>
2023-02-28 00:58:07 +0000

    Rewrite dotnet feature

--- test/dotnet/test.sh
+++ test/dotnet/test.sh
@@ -6,16 +6,17 @@ set -e
 source dev-container-features-test-lib
 
 # Definition specific tests
-check "dotnet" dotnet --info
-check "sdks" dotnet --list-sdks
+check "info" dotnet --info
+check "list-sdks" dotnet --list-sdks
 check "version" dotnet --version
 
-echo "Validating expected version present..."
-check "some major version of dotnet 7 is installed" bash -c "dotnet --version |  grep '7\.[0-9]*\.[0-9]*'"
+# Make sure the symlink works
+check "current link info" /usr/local/dotnet/current/dotnet --info
+check "current link sdk directory" ls -l /usr/local/dotnet/current/sdk
 
-# Verify current symlink exists and works
-check "current link dotnet" /usr/local/dotnet/current/dotnet --info
-check "current link sdk" ls -l /usr/local/dotnet/current/sdk
+# TODO: Installer script defaults to .NET 6 as that's LTS. Update this test or remove it.
+# echo "Validating expected version present..."
+# check "some major version of dotnet 7 is installed" bash -c "dotnet --version |  grep '7\.[0-9]*\.[0-9]*'"
 
 # Report result
 reportResults
