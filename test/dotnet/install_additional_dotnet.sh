018003a29157534319297c6d55355e54333637d1
JP Ungaretti <git@jungaretti.com>
2023-03-01 07:34:31 +0000

    Update additional version test

--- test/dotnet/install_additional_dotnet.sh
+++ test/dotnet/install_additional_dotnet.sh
@@ -5,13 +5,13 @@ set -e
 # Optional: Import test library
 source dev-container-features-test-lib
 
-check "dotnet version 6.0.301 installed as default" bash -c "dotnet --version | grep 6.0.301"
-check "dotnet version 5.0 installed"  bash -c "ls -l /usr/local/dotnet | grep 5.0"
-check "dotnet version 3.1.420 installed"  bash -c "ls -l /usr/local/dotnet | grep 3.1.420"
+./test.sh
 
-# Verify current symlink exists and works
-check "current link dotnet" /usr/local/dotnet/current/dotnet --info
-check "current link sdk" ls -l /usr/local/dotnet/current/sdk
+check "version 6.0.405 installed" bash -c 'dotnet --list-sdks | grep 6.0.405'
+check "version 3.1.416 installed" bash -c 'dotnet --list-sdks | grep 3.1.416'
+
+# primary version plus 2 additional versions
+check "latest version installed" bash -c 'test "$(dotnet --list-sdks | wc -l)" -eq 3'
 
 # Report result
 reportResults
