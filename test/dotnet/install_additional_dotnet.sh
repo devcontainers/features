b00c0796028f82a4db0b43988cf57e33c5a95691
JP Ungaretti <git@jungaretti.com>
2023-03-01 22:13:40 +0000

    Update additional versions scenario

--- test/dotnet/install_additional_dotnet.sh
+++ test/dotnet/install_additional_dotnet.sh
@@ -7,9 +7,10 @@ source dev-container-features-test-lib
 ./test.sh
 
 check "version 6.0.405 installed" bash -c 'dotnet --list-sdks | grep 6.0.405'
+check "version 6.0.402 installed" bash -c 'dotnet --list-sdks | grep 6.0.402'
 check "version 3.1.416 installed" bash -c 'dotnet --list-sdks | grep 3.1.416'
 
 # Primary version plus 2 additional versions
-check "latest version installed" bash -c 'test "$(dotnet --list-sdks | wc -l)" -eq 3'
+check "latest version installed" bash -c 'test "$(dotnet --list-sdks | wc -l)" -eq 4'
 
 reportResults
