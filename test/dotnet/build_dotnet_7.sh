dcbd98f08ea034f90c06946af95387d55550efcb
JP Ungaretti <git@jungaretti.com>
2023-03-01 19:19:02 +0000

    Run project tests as non-root user

--- test/dotnet/build_dotnet_7.sh
+++ test/dotnet/build_dotnet_7.sh
@@ -4,6 +4,8 @@ set -e
 
 source dev-container-features-test-lib
 
+check "non-root user" test "$(id -u)" -ne 0
+
 pushd example_project
 check "dotnet restore" dotnet restore
 check "dotnet build" dotnet build
