a4746b2feb341357a30429294881dfc8fd6707d0
JP Ungaretti <git@jungaretti.com>
2023-03-01 07:39:09 +0000

    Update version tests

--- test/dotnet/install_dotnet_6_focal.sh
+++ test/dotnet/install_dotnet_6_focal.sh
@@ -5,13 +5,9 @@ set -e
 # Optional: Import test library
 source dev-container-features-test-lib
 
-check "dotnet sdks" dotnet --list-sdks
-check "some major version of dotnet 6 is installed" bash -c "dotnet --list-sdks |  grep '6\.[0-9]*\.[0-9]*'"
-check "dotnet version 6 installed"  bash -c "ls -l /usr/share/dotnet/sdk | grep '6\.[0-9]*\.[0-9]*'"
+./test.sh
 
-# Verify current symlink exists and works
-check "current link dotnet" /usr/local/dotnet/current/dotnet --info
-check "current link sdk" ls -l /usr/local/dotnet/current/sdk
+check "some major version of dotnet 6 is installed" bash -c "dotnet --list-sdks | grep '6\.[0-9]*\.[0-9]*'"
 
 # Report result
 reportResults
