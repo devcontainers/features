0046718ffe44bf40875fa0190bd3ef68d65a8b8a
Felipe Santos <felipecassiors@gmail.com>
2023-03-30 20:16:56 -0300

    Fix possible test issue

--- test/docker-outside-of-docker/docker_init.sh
+++ test/docker-outside-of-docker/docker_init.sh
@@ -11,7 +11,7 @@ check "log-contents-for-success" bash -c "cat /tmp/vscr-docker-from-docker.log |
 check "log-contents" bash -c "cat /tmp/vscr-docker-from-docker.log | grep 'Proxying /var/run/docker-host.sock to /var/run/docker.sock for vscode'"
 check "docker-ps" bash -c "docker ps"
 check "docker-compose" bash -c "docker-compose --version"
-check "docker compose" bash -c "docker compose --version"
+check "docker compose" bash -c "docker compose version"
 
 # Report result
 reportResults
