1ea825c958a7cc9461607a5443c8f58a7cd4ec6f
Felipe Santos <felipecassiors@gmail.com>
2023-03-30 20:21:32 -0300

    Fix inconsistency

--- test/docker-outside-of-docker/docker_init.sh
+++ test/docker-outside-of-docker/docker_init.sh
@@ -10,9 +10,9 @@ check "log-exists" bash -c "ls /tmp/vscr-docker-from-docker.log"
 check "log-contents-for-success" bash -c "cat /tmp/vscr-docker-from-docker.log | grep 'Success'"
 check "log-contents" bash -c "cat /tmp/vscr-docker-from-docker.log | grep 'Proxying /var/run/docker-host.sock to /var/run/docker.sock for vscode'"
 check "docker-ps" bash -c "docker ps"
-check "docker-buildx" bash -c "docker buildx version"
-check "docker-compose" bash -c "docker-compose --version"
+check "docker buildx" bash -c "docker buildx version"
 check "docker compose" bash -c "docker compose version"
+check "docker-compose" bash -c "docker-compose --version"
 
 # Report result
 reportResults
