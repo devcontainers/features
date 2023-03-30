8be8980690cb192b229c2786beadf4595c124d38
Felipe Santos <felipecassiors@gmail.com>
2023-03-30 20:20:44 -0300

    Add buildx test to init

--- test/docker-outside-of-docker/docker_init.sh
+++ test/docker-outside-of-docker/docker_init.sh
@@ -10,6 +10,7 @@ check "log-exists" bash -c "ls /tmp/vscr-docker-from-docker.log"
 check "log-contents-for-success" bash -c "cat /tmp/vscr-docker-from-docker.log | grep 'Success'"
 check "log-contents" bash -c "cat /tmp/vscr-docker-from-docker.log | grep 'Proxying /var/run/docker-host.sock to /var/run/docker.sock for vscode'"
 check "docker-ps" bash -c "docker ps"
+check "docker-buildx" bash -c "docker buildx version"
 check "docker-compose" bash -c "docker-compose --version"
 check "docker compose" bash -c "docker compose version"
 
