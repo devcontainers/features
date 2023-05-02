e1cb6a4f86eae6432c94b385351216f79e204d81
m-roberts <2947595+m-roberts@users.noreply.github.com>
2023-05-02 10:25:24 +0100

    Add test conditions for docker-outside-of-docker

--- test/docker-outside-of-docker/docker_build_no_compose.sh
+++ test/docker-outside-of-docker/docker_build_no_compose.sh
@@ -9,8 +9,8 @@ source dev-container-features-test-lib
 check "docker-buildx" docker buildx version
 check "docker-build" docker build ./
 
-check "installs docker-compose v1 install" bash -c "type docker-compose"
-check "installs compose-switch" bash -c "[[ -f /usr/local/bin/compose-switch ]]"
+check "not installing compose skips docker-compose v1 install" bash -c "! type docker-compose"
+check "not installing compose skips compose-switch" bash -c "[[ ! -f /usr/local/bin/compose-switch ]]"
 
 # Report result
 reportResults
