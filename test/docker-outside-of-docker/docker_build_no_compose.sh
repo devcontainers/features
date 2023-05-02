9e9e70b5370e5e635593599a900dab0c9eb42ec9
m-roberts <2947595+m-roberts@users.noreply.github.com>
2023-05-02 19:12:45 +0100

    Test for correct path

--- test/docker-outside-of-docker/docker_build_no_compose.sh
+++ test/docker-outside-of-docker/docker_build_no_compose.sh
@@ -10,7 +10,6 @@ check "docker-buildx" docker buildx version
 check "docker-build" docker build ./
 
 check "not installing compose skips docker-compose v1 install" bash -c "! type docker-compose"
-check "not installing compose skips compose-switch" bash -c "[[ ! -f /usr/local/bin/compose-switch ]]"
 
 # Report result
 reportResults
