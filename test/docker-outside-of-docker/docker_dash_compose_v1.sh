564988613d15afd031e97aa4b72d1a48f94c3651
m-roberts <2947595+m-roberts@users.noreply.github.com>
2023-05-02 19:00:21 +0100

    Put into correct test

--- test/docker-outside-of-docker/docker_dash_compose_v1.sh
+++ test/docker-outside-of-docker/docker_dash_compose_v1.sh
@@ -9,7 +9,5 @@ source dev-container-features-test-lib
 check "docker compose" bash -c "docker compose version | grep -E '2.[0-9]+.[0-9]+'"
 check "docker-compose" bash -c "docker-compose --version | grep -E '1.[0-9]+.[0-9]+'"
 
-check "installs compose-switch" bash -c "[[ -f /usr/local/bin/compose-switch ]]"
-
 # Report result
 reportResults
