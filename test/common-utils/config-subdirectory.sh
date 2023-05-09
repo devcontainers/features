14e202d178da972cc10bd44f5e48db723a9eae89
Stephen A. Imhoff <clockwork-muse@outlook.com>
2023-05-09 00:47:00 +0000

    Remove extra debug line.

--- test/common-utils/config-subdirectory.sh
+++ test/common-utils/config-subdirectory.sh
@@ -6,7 +6,6 @@ set -e
 source dev-container-features-test-lib
 
 # Definition specific tests
-# check "owned config directory" bash -c "ls -ld .vscode | awk '{print $3}' | grep 'devcontainer'"
 check "owned config directory" ls -ld ~/.config/subdirectory | awk '{print $3}' | grep 'devcontainer'
 check "owned config directory" ls -ld ~/.config | awk '{print $3}' | grep 'devcontainer'
 
