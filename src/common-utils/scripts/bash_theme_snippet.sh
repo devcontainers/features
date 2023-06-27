c408d345524651c8c0224520e193454c18b88a81
Marco Casaroli <marco.casaroli@gmail.com>
2023-06-27 19:26:08 +0200

    Use `\$` as the final character of `PS1`

--- src/common-utils/scripts/bash_theme_snippet.sh
+++ src/common-utils/scripts/bash_theme_snippet.sh
@@ -18,7 +18,7 @@ __bash_prompt() {
         fi`'
     local lightblue='\[\033[1;34m\]'
     local removecolor='\[\033[0m\]'
-    PS1="${userpart} ${lightblue}\w ${gitbranch}${removecolor}\$ "
+    PS1="${userpart} ${lightblue}\w ${gitbranch}${removecolor}\\$ "
     unset -f __bash_prompt
 }
 __bash_prompt
