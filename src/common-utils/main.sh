685fa2e0f151b4545b444135a5ef6877de20e851
Krikchai Pongtaveewould <krikchai.p@gmail.com>
2023-08-03 05:12:14 +0700

    Fix: common-utils configureZshAsDefaultShell option not working on alpine linux image (#557)
    * alpine defaultZsh fix
    
    * common-utils: replace pam.d/chsh permission with `sufficient`
    
    * common-utils: revert match auth sufficient
    
    * common-utils: append auth string instead of replacing a whole file
    
    * fix grep 2nd param
    
    * fix: line checking condition

--- src/common-utils/main.sh
+++ src/common-utils/main.sh
@@ -437,6 +437,14 @@ if [ "${INSTALL_ZSH}" = "true" ]; then
     fi
 
     if [ "${CONFIGURE_ZSH_AS_DEFAULT_SHELL}" == "true" ]; then
+        # Fixing chsh always asking for a password on alpine linux
+        # ref: https://askubuntu.com/questions/812420/chsh-always-asking-a-password-and-get-pam-authentication-failure.
+        if [ ! -f "/etc/pam.d/chsh" ] || ! grep -Eq '^auth(.*)pam_rootok\.so$' /etc/pam.d/chsh; then
+            echo "auth sufficient pam_rootok.so" >> /etc/pam.d/chsh
+        elif [[ -n "$(awk '/^auth(.*)pam_rootok\.so$/ && !/^auth[[:blank:]]+sufficient[[:blank:]]+pam_rootok\.so$/' /etc/pam.d/chsh)" ]]; then
+            awk '/^auth(.*)pam_rootok\.so$/ { $2 = "sufficient" } { print }' /etc/pam.d/chsh > /tmp/chsh.tmp && mv /tmp/chsh.tmp /etc/pam.d/chsh
+        fi
+
         chsh --shell /bin/zsh ${USERNAME}
     fi
 
