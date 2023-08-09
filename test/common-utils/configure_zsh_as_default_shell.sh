e29f2297ad89992fc760b6249a50ea14d78619d8
natured_amends0p <natured_amends0p@icloud.com>
2023-08-09 06:36:39 +0000

    Testing for using devcontainer .zshrc template file.

--- test/common-utils/configure_zsh_as_default_shell.sh
+++ test/common-utils/configure_zsh_as_default_shell.sh
@@ -7,6 +7,8 @@ source dev-container-features-test-lib
 
 # Definition specific tests
 check "default-shell-is-zsh" bash -c "getent passwd $(whoami) | awk -F: '{ print $7 }' | grep '/bin/zsh'"
+# check it overrides the ~/.zshrc with default dev containers template
+check "default-zshrc-dev-container-template" bashc -c "cat ~/.zshrc | grep ZSH_THEME=\"devcontainer\""
 
 # Report result
 reportResults
