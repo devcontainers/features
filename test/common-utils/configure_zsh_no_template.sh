875003398983a18870ed0b95bef251572f8090e2
naturedamends <120284608+naturedamends@users.noreply.github.com>
2023-08-11 23:14:43 +0100

    Update configure_zsh_no_template.sh

--- test/common-utils/configure_zsh_no_template.sh
+++ test/common-utils/configure_zsh_no_template.sh
@@ -7,7 +7,6 @@ source dev-container-features-test-lib
 
 # Definition specific tests
 function file_not_overridden() {
-    cat /home/natured.log
     cat ~/.zshrc | grep 'alias fnomockalias=' | grep testingmock
 }
 check "default-zsh-with-no-zshrc" file_not_overridden
