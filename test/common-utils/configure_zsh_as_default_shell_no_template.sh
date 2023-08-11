eed375a65df9f8088b719551fadad84dc39b4a56
naturedamends <120284608+naturedamends@users.noreply.github.com>
2023-08-11 09:32:48 +0100

    Update configure_zsh_as_default_shell_no_template.sh

--- test/common-utils/configure_zsh_as_default_shell_no_template.sh
+++ test/common-utils/configure_zsh_as_default_shell_no_template.sh
@@ -6,7 +6,7 @@ set -e
 source dev-container-features-test-lib
 
 # Definition specific tests
-check "default-shell-is-zsh-with-no-template" bash -c "getent passwd $(whoami) | awk -F: '{ print $7 }' | grep '/bin/zsh'; [ ! -e ~/.zshrc ]"
+check "default-shell-is-zsh-with-no-template" bash -e -x -c "getent passwd $(whoami) | awk -F: '{ print $7 }' | grep '/bin/zsh'; [ ! -e ~/.zshrc ]"
 
 # Report result
 reportResults
