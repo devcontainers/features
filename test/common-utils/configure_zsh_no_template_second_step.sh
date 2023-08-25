0668db5edea56a0d57575db127b793f59b489e7b
naturedamends <120284608+naturedamends@users.noreply.github.com>
2023-08-26 00:39:32 +0100

    Commonutils: chown: cannot access '/home/vscode/.zshrc': No such file or directory (#662)
    * Update main.sh
    
    * Update devcontainer-feature.json
    
    * Test install as non root user.
    
    ---------
    
    Co-authored-by: Samruddhi Khandale <skhandale@microsoft.com>

--- test/common-utils/configure_zsh_no_template_second_step.sh
+++ test/common-utils/configure_zsh_no_template_second_step.sh
@@ -4,12 +4,11 @@ set -e
 
 # Optional: Import test library
 source dev-container-features-test-lib
-
 # Definition specific tests
 function file_not_overridden() {
-    cat ~/.zshrc | grep 'alias fnomockalias=' | grep testingmock
+    cat $1 | grep 'alias fnomockalias=' | grep testingmock
 }
-check "default-zsh-with-no-zshrc" file_not_overridden
+check "default-zsh-with-no-zshrc" file_not_overridden /home/devcontainer/.zshrc
 
 # Report result
 reportResults
