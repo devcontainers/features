95028c4112c73f59f3aa43cfef09749cdd6681fc
Steven Liekens <steven.liekens@gmail.com>
2023-08-27 18:43:16 +0000

    Inline install_version function

--- src/dotnet/install.sh
+++ src/dotnet/install.sh
@@ -96,9 +96,61 @@ done
 # icu-devtools includes dependencies for .NET
 check_packages wget ca-certificates icu-devtools
 
+# Quick options reminder for dotnet-install.sh:
+#
+# --version: 'latest' (default) or an exact version in the form 'A.B.C' like '6.0.412'
+# --channel: 'LTS' (default), 'STS', a two-part version in the form 'A.B' like '6.0' or three-part form 'A.B.Cxx' like '6.0.1xx'
+# --quality: 'daily', 'signed', 'validated', 'preview' or 'GA'
+#
+# The channel option is only used when version is 'latest' because an exact version overrides the channel option
+# The quality option is only used when channel is 'A.B' or 'A.B.Cxx' because it can't be used with STS or LTS
 
-for version in "${versions[@]}"; do
-    install_version $version
+# This script aims to reduce these combinations of options to a single 'version' input
+for inputVersion in "${versions[@]}"; do
+    version=""
+    channel=""
+    if [[ "$inputVersion" == "latest" ]]; then
+        # Fetch the latest version manually, because dotnet-install.sh does not support it directly
+        version=$(fetch_latest_sdk_version)
+        channel=""
+    elif [[ "$inputVersion" == "lts" ]]; then
+        # When user input is 'lts'
+        # Then version=latest, channel=LTS
+        version="latest"
+        channel="LTS"
+    elif [[ "$inputVersion" =~ ^[0-9]+\.[0-9]+$ ]]; then
+        # When user input is form 'A.B' like '3.1'
+        # Then version=latest, channel=3.1
+        version="latest"
+        channel="$inputVersion"
+    elif [[ "$inputVersion" =~ ^[0-9]+\.[0-9]+\.[0-9]xx$ ]]; then
+        # When user input is form 'A.B.Cxx' like '6.0.4xx'
+        # Then version=latest, channel=6.0.4xx
+        version="latest"
+        channel="$inputVersion"
+    else
+        # Assume version is an exact version string like '6.0.412' or '8.0.100-rc.1.23371.5''
+        version="$inputVersion"
+        channel=""
+    fi
+
+    # Valid examples
+    #
+    # dotnet-install.sh [--version latest] [--channel LTS]
+    # dotnet-install.sh [--version latest] --channel STS
+    # dotnet-install.sh [--version latest] --channel 6.0 [--quality GA]
+    # dotnet-install.sh [--version latest] --channel 6.0.4xx [--quality GA]
+    # dotnet-install.sh [--version latest] --channel 8.0 --quality preview
+    # dotnet-install.sh [--version latest] --channel 8.0 --quality daily
+    # dotnet-install.sh --version 6.0.412
+    
+    # Currently this script does not make it possible to qualify the version, 'GA' is always implied
+    echo "Executing $DOTNET_INSTALL_SCRIPT --install-dir $DOTNET_INSTALL_DIR --version $version --channel $channel --no-path"
+    "$DOTNET_INSTALL_SCRIPT" \
+        --install-dir "$DOTNET_INSTALL_DIR" \
+        --version "$version" \
+        --channel "$channel" \
+        --no-path
 done
 
 # Clean up
