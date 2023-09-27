cbb6ae5dcb3f164a7f9042ac24157f30707a53fa
Pablo Ulloa <pablo.ulloac@usach.cl>
2023-09-27 02:03:03 +0000

    reduced conditions to improve readability

--- src/common-utils/main.sh
+++ src/common-utils/main.sh
@@ -407,16 +407,14 @@ fi
 
 if [ "${USERNAME}" = "root" ]; then
     user_home="/root"
+# Check if user already has a home directory other than /home/${USERNAME}
+elif [ "/home/${USERNAME}" != $( getent passwd $USERNAME | cut -d: -f6 ) ]; then
+    user_home=$( getent passwd $USERNAME | cut -d: -f6 )
 else
-    # Check if user already has a home directory other than /home/${USERNAME}
-    if [ "/home/${USERNAME}" != $( getent passwd $USERNAME | cut -d: -f6 ) ]; then
-        user_home=$( getent passwd $USERNAME | cut -d: -f6 )
-    else
-        user_home="/home/${USERNAME}"
-        if [ ! -d "${user_home}" ]; then
-            mkdir -p "${user_home}"
-            chown ${USERNAME}:${group_name} "${user_home}"
-        fi
+    user_home="/home/${USERNAME}"
+    if [ ! -d "${user_home}" ]; then
+        mkdir -p "${user_home}"
+        chown ${USERNAME}:${group_name} "${user_home}"
     fi
 fi
 
