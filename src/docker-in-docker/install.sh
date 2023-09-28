52903a01fbf96cfa6cfb85a55683f15d2533bc70
Samruddhi Khandale <skhandale@microsoft.com>
2023-09-28 22:39:04 +0000

    pkill only if container didn't start at first

--- src/docker-in-docker/install.sh
+++ src/docker-in-docker/install.sh
@@ -362,10 +362,6 @@ EOF
 tee -a /usr/local/share/docker-init.sh > /dev/null \
 << 'EOF'
 dockerd_start="AZURE_DNS_AUTO_DETECTION=${AZURE_DNS_AUTO_DETECTION} DOCKER_DEFAULT_ADDRESS_POOL=${DOCKER_DEFAULT_ADDRESS_POOL} $(cat << 'INNEREOF'
-    # Stop dockerd and containerd in case they are already running
-    pkill dockerd
-    pkill containerd
-    
     # explicitly remove dockerd and containerd PID file to ensure that it can start properly if it was stopped uncleanly
     find /run /var/run -iname 'docker*.pid' -delete || :
     find /run /var/run -iname 'container*.pid' -delete || :
@@ -472,6 +468,15 @@ do
     
     if [ "${docker_ok}" != "true" ]; then
         echo "(*) Failed to start docker, retrying..."
+
+        # Stop dockerd and containerd in case they are already running
+        if [ "$(id -u)" -ne 0 ]; then
+            sudo pkill dockerd
+            sudo pkill containerd
+        else
+            pkill dockerd
+            pkill containerd
+        fi
     fi
     
     retry_docker_start_count=`expr $retry_docker_start_count + 1`
