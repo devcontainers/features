ef83cfcd82b506c8e1efd73b3f68b0c620a4a6ab
Thomas Owens <thomas.j.owens@gmail.com>
2023-11-25 00:07:33 +0000

    More specific no versions test case

--- test/ruby/no_version.sh
+++ test/ruby/no_version.sh
@@ -7,6 +7,7 @@ source dev-container-features-test-lib
 
 # Definition specific tests
 check "rvm" rvm --version
+check "no rvm rubies" bash -c "rvm list | fgrep 'No rvm rubies installed yet'"
 
 # Report result
 reportResults
