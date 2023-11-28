c53818c5ff2e6029a1c2ee4cba7a2f6f4a78e199
Alexander Smolyakov <v-asmoliakov@microsoft.com>
2023-11-28 13:06:45 +0400

    Introduce `packages` option
    - Create `utils.sh` files;
    - Move `sudo_if` to utils.sh`;
    - Replace `install_user_package` with `install_python_package` and move function to utils.sh`;
    - Add logic to handle `packages` input;

---
+++ src/python/utils.sh
@@ -0,0 +1,33 @@
+sudo_if() {
+    COMMAND="$*"
+    if [ "$(id -u)" -eq 0 ] && [ "$USERNAME" != "root" ]; then
+        su - "$USERNAME" -c "$COMMAND"
+    else
+        $COMMAND
+    fi
+}
+
+install_python_package() {
+    INSTALL_UNDER_ROOT="$1"
+    PYTHON_PATH="$2"
+    PACKAGE="$3"
+    VERSION="${4:-""}"
+
+    sudo_if "${PYTHON_PATH}" -m pip uninstall --yes "${PACKAGE}"
+
+    install_command="-m pip install --upgrade --no-cache-dir "
+
+    if [ "$INSTALL_UNDER_ROOT" = false ]; then
+        install_command+="--user "
+    fi
+
+    install_command+="${PACKAGE}"
+
+    if [ ! -z "${VERSION}" ]; then
+      install_command+="==${VERSION}"
+    fi
+
+    sudo_if "${PYTHON_PATH} ${install_command}"
+
+    sudo_if "${PYTHON_PATH}" -m pip --no-python-version-warning show "${PACKAGE}"
+}
