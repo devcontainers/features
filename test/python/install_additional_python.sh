e884958dd9fd487d686fe004dfa2585956c1d22b
Bas Nijholt <bas@nijho.lt>
2023-12-15 09:36:10 -0800

    Add check for `which ruff`

--- test/python/install_additional_python.sh
+++ test/python/install_additional_python.sh
@@ -37,6 +37,7 @@ check "which pycodestyle" bash -c "which pycodestyle | grep /usr/local/py-utils/
 check "which pydocstyle" bash -c "which pydocstyle | grep /usr/local/py-utils/bin/pydocstyle"
 check "which pylint" bash -c "which pylint | grep /usr/local/py-utils/bin/pylint"
 check "which pytest" bash -c "which pytest | grep /usr/local/py-utils/bin/pytest"
+check "which ruff" bash -c "which ruff | grep /usr/local/py-utils/bin/ruff"
 
 # Report result
 reportResults
