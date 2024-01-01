ca4a05467f98065e7d2e28564ed2937fe0a15527
nohzafk <149959021+nohzafk@users.noreply.github.com>
2024-01-01 19:18:02 +0800

    add option `useAttrPath` to support `nix-env -iA`

--- src/nix/post-install-steps.sh
+++ src/nix/post-install-steps.sh
@@ -2,10 +2,29 @@
 set -e
 echo "(*) Executing post-installation steps..."
 
+# if not starts with "nixpkgs." add it as prefix to package name
+add_nixpkgs_prefix() {
+  local packages=$1
+  local -a addr
+  IFS=' ' read -ra addr <<<"$packages"
+  for i in "${!addr[@]}"; do
+    if [[ ${addr[i]} != nixpkgs.* ]]; then
+      addr[i]="nixpkgs.${addr[i]}"
+    fi
+  done
+  IFS=' ' echo "${addr[*]}"
+}
+
 # Install list of packages in profile if specified.
 if [ ! -z "${PACKAGES}" ] && [ "${PACKAGES}" != "none" ]; then
+  if [ "${USEATTRPATH}" = "true" ]; then
+    PACKAGES=$(add_nixpkgs_prefix "$PACKAGES")
     echo "Installing packages \"${PACKAGES}\" in profile..."
-    nix-env --install ${PACKAGES}
+    nix-env -iA "${PACKAGES}"
+  else
+    echo "Installing packages \"${PACKAGES}\" in profile..."
+    nix-env --install "${PACKAGES}"
+  fi
 fi
 
 # Install Nix flake in profile if specified
