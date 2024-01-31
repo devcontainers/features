4d3333e2d115a0e6a3d6ed810ecf1557d7f13e6a
Samruddhi Khandale <skhandale@microsoft.com>
2024-01-31 19:11:11 +0000

    [docker-in-docker] : Bug fix - Buildx installation when moby:false

--- test/docker-in-docker/docker_build_older.sh
+++ test/docker-in-docker/docker_build_older.sh
@@ -8,6 +8,8 @@ source dev-container-features-test-lib
 # Definition specific tests
 check "docker-buildx" docker buildx version
 check "docker-build" docker build ./
+check "docker-buildx" bash -c "docker buildx version"
+check "docker-buildx-path" bash -c "ls -la /usr/libexec/docker/cli-plugins/docker-buildx"
 
 # Report result
 reportResults
