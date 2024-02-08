8eeb65108ae1ea075360d2eea20f3d095c3600fb
gauravsaini04 <147703805+gauravsaini04@users.noreply.github.com>
2024-02-08 02:11:46 +0000

    changes according to comments by Samruddhi

--- test/common-utils/bookworm.sh
+++ test/common-utils/bookworm.sh
@@ -5,6 +5,37 @@ set -e
 # Optional: Import test library
 source dev-container-features-test-lib
 
+FAILED=()
+echoStderr()
+{
+    echo "$@" 1>&2
+}
+
+checkOSPackages() {
+    LABEL=$1
+    shift
+    echo -e "\n🧪 Testing $LABEL"
+    if dpkg-query --show -f='${Package}: ${Version}\n' "$@"; then 
+        echo "✅  Passed!"
+        return 0
+    else
+        echoStderr "❌ $LABEL check failed."
+        FAILED+=("$LABEL")
+        return 1
+    fi
+}
+
+checkCommon()
+{
+    PACKAGE_LIST="manpages-posix \
+        manpages-posix-dev"
+
+    checkOSPackages "Installation of manpages-posix and manpages-posix-dev (non-free)" ${PACKAGE_LIST}
+}
+
+# Check for manpages-posix, manpages-posix-dev non-free packages
+checkCommon
+
 # Definition specific tests
 . /etc/os-release
 check "non-root user" test "$(whoami)" = "devcontainer"
