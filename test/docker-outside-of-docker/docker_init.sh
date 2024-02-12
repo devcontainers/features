1d8baad70821fe8a332287e278f4eebd9696f4f8
Samruddhi Khandale <skhandale@microsoft.com>
2024-02-12 18:01:41 +0000

    fix test

--- test/docker-outside-of-docker/docker_init.sh
+++ test/docker-outside-of-docker/docker_init.sh
@@ -5,9 +5,6 @@ set -e
 # Optional: Import test library
 source dev-container-features-test-lib
 
-echo "contents cat /tmp/vscr-docker-from-docker.log"
-cat cat /tmp/vscr-docker-from-docker.log
-
 check "docker buildx" bash -c "docker buildx version"
 check "docker compose" bash -c "docker compose version"
 check "docker-compose" bash -c "docker-compose --version"
@@ -16,11 +13,7 @@ check "docker-init-exists" bash -c "ls /usr/local/share/docker-init.sh"
 check "log-exists" bash -c "ls /tmp/vscr-docker-from-docker.log"
 check "log-contents-for-success" bash -c "cat /tmp/vscr-docker-from-docker.log | grep 'Success'"
 
-sleep 5s
-echo "contents cat /tmp/vscr-docker-from-docker.log"
-cat cat /tmp/vscr-docker-from-docker.log
-
-check "log-contents" bash -c "cat /tmp/vscr-docker-from-docker.log | grep 'Proxying /var/run/docker-host.sock to /var/run/docker.sock for vscode'"
+check "log-contents" bash -c "cat /tmp/vscr-docker-from-docker.log | grep 'Ensuring vscode has access to /var/run/docker-host.sock via /var/run/docker.sock"
 check "docker-ps" bash -c "docker ps >/dev/null"
 
 # Report result
