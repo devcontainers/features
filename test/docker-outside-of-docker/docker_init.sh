ea05a2f3105e5d95b20a86ddf1a307c9205d29e1
Samruddhi Khandale <skhandale@microsoft.com>
2024-02-12 17:51:20 +0000

    test debugging

--- test/docker-outside-of-docker/docker_init.sh
+++ test/docker-outside-of-docker/docker_init.sh
@@ -5,6 +5,9 @@ set -e
 # Optional: Import test library
 source dev-container-features-test-lib
 
+echo "contents cat /tmp/vscr-docker-from-docker.log"
+cat cat /tmp/vscr-docker-from-docker.log
+
 check "docker buildx" bash -c "docker buildx version"
 check "docker compose" bash -c "docker compose version"
 check "docker-compose" bash -c "docker-compose --version"
@@ -12,6 +15,11 @@ check "docker-compose" bash -c "docker-compose --version"
 check "docker-init-exists" bash -c "ls /usr/local/share/docker-init.sh"
 check "log-exists" bash -c "ls /tmp/vscr-docker-from-docker.log"
 check "log-contents-for-success" bash -c "cat /tmp/vscr-docker-from-docker.log | grep 'Success'"
+
+sleep 5s
+echo "contents cat /tmp/vscr-docker-from-docker.log"
+cat cat /tmp/vscr-docker-from-docker.log
+
 check "log-contents" bash -c "cat /tmp/vscr-docker-from-docker.log | grep 'Proxying /var/run/docker-host.sock to /var/run/docker.sock for vscode'"
 check "docker-ps" bash -c "docker ps >/dev/null"
 
