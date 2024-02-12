0bf8c0e345713fb4f622a8f99351469164007f3a
Samruddhi Khandale <skhandale@microsoft.com>
2024-02-12 18:09:12 +0000

    nit

--- test/docker-outside-of-docker/docker_init.sh
+++ test/docker-outside-of-docker/docker_init.sh
@@ -13,7 +13,7 @@ check "docker-init-exists" bash -c "ls /usr/local/share/docker-init.sh"
 check "log-exists" bash -c "ls /tmp/vscr-docker-from-docker.log"
 check "log-contents-for-success" bash -c "cat /tmp/vscr-docker-from-docker.log | grep 'Success'"
 
-check "log-contents" bash -c "cat /tmp/vscr-docker-from-docker.log | grep 'Ensuring vscode has access to /var/run/docker-host.sock via /var/run/docker.sock"
+check "log-contents" bash -c "cat /tmp/vscr-docker-from-docker.log | grep 'Ensuring vscode has access to /var/run/docker-host.sock via /var/run/docker.sock'"
 check "docker-ps" bash -c "docker ps >/dev/null"
 
 # Report result
