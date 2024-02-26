f843609601b834b05087b07b840e8f68d3441fea
Samruddhi Khandale <skhandale@microsoft.com>
2024-02-26 18:47:54 +0000

    validate "compose-switch" installation

--- test/docker-in-docker/docker_compose_v1.sh
+++ test/docker-in-docker/docker_compose_v1.sh
@@ -8,6 +8,7 @@ source dev-container-features-test-lib
 # Definition specific tests
 
 check "docker-compose" bash -c "docker-compose version | grep -E '1.[0-9]+.[0-9]+'"
+check "installs compose-switch" bash -c "[[ -f /usr/local/bin/compose-switch ]]"
 
 # Report result
 reportResults
