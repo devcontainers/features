dcf921bd5cc50b34de7398be890f36a4ddc38dbf
Samruddhi Khandale <skhandale@microsoft.com>
2024-02-26 21:52:11 +0000

    fix test

--- test/docker-in-docker/docker_compose_v1.sh
+++ test/docker-in-docker/docker_compose_v1.sh
@@ -8,7 +8,6 @@ source dev-container-features-test-lib
 # Definition specific tests
 
 check "docker-compose" bash -c "docker-compose version | grep -E '1.[0-9]+.[0-9]+'"
-check "installs compose-switch" bash -c "[[ -f /usr/local/bin/compose-switch ]]"
 
 # Report result
 reportResults
