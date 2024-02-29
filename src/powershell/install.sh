6a7a972f58b210a56f364b4779a03903e6d1e08e
Nebula <40148908+nebula-it@users.noreply.github.com>
2024-02-28 16:00:23 -0800

    Update install.sh

--- src/powershell/install.sh
+++ src/powershell/install.sh
@@ -163,10 +163,26 @@ if [ ${#POWERSHELL_MODULES[@]} -gt 0 ]; then
     done
 fi
 
+ # Determine the appropriate non-root user 
+ if [ "${USERNAME}" = "auto" ] || [ "${USERNAME}" = "automatic" ]; then 
+     USERNAME="" 
+     POSSIBLE_USERS=("vscode" "node" "codespace" "$(awk -v val=1000 -F ":" '$3==val{print $1}' /etc/passwd)") 
+     for CURRENT_USER in "${POSSIBLE_USERS[@]}"; do 
+         if id -u ${CURRENT_USER} > /dev/null 2>&1; then 
+             USERNAME=${CURRENT_USER} 
+             break 
+         fi 
+     done 
+     if [ "${USERNAME}" = "" ]; then 
+         USERNAME=root 
+     fi 
+ elif [ "${USERNAME}" = "none" ] || ! id -u ${USERNAME} > /dev/null 2>&1; then 
+     USERNAME=root 
+ fi 
 # If URL for powershell profile is provided, download it to '/opt/microsoft/powershell/7/profile.ps1'
 if [ -n "$POWERSHELL_PROFILE_URL" ]; then
     echo "Downloading PowerShell Profile from: $POWERSHELL_PROFILE_URL"
-    sudo su -c 'curl -sSL -o "/opt/microsoft/powershell/7/profile.ps1" "$POWERSHELL_PROFILE_URL"'
+    su ${USERNAME} -c 'curl -sSL -o "/opt/microsoft/powershell/7/profile.ps1" "$POWERSHELL_PROFILE_URL"'
 fi
 
 # Clean up
