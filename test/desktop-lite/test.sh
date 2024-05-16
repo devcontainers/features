c0a169d9c80627430b11157af1c31b6bee5c7413
Gaurav Saini <147703805+gauravsaini04@users.noreply.github.com>
2024-05-16 00:07:28 +0000

    changes based on review comments..

--- test/desktop-lite/test.sh
+++ test/desktop-lite/test.sh
@@ -17,8 +17,10 @@ checkOSPackage() {
     # Check if the package exists and retrieve its exact version
     if [ "$(dpkg-query -W -f='${Status}' "$PACKAGE_NAME" 2>/dev/null | grep -c "ok installed")" -eq 1 ]; then
         echo "✅  Package '$PACKAGE_NAME' is installed."
+        exit 0
     else
         echo "❌ Package '$PACKAGE_NAME' is not installed."
+        exit 1
     fi
 }
 
@@ -26,8 +28,16 @@ check "desktop-init-exists" bash -c "ls /usr/local/share/desktop-init.sh"
 check "log-exists" bash -c "ls /tmp/container-init.log"
 check "fluxbox-exists" bash -c "ls -la ~/.fluxbox"
 
-checkOSPackage "if libasound2 exists !" "libasound2"
-checkOSPackage "if libasound2-dev exists !" "libasound2-dev"
+. /etc/os-release
+if [ "${ID}" = "ubuntu" ]; then
+    if [ "${VERSION_CODENAME}" = "noble" ]; then
+        checkOSPackage "if libasound2-dev exists !" "libasound2-dev"
+    else 
+        checkOSPackage "if libasound2 exists !" "libasound2"
+    fi
+else 
+    checkOSPackage "if libasound2 exists !" "libasound2"
+fi
 
 # Report result
 reportResults
