5825c0de6726012ed2945b0a4b3d21adbf96a3b5
Prathamesh Zarkar <159782310+prathameshzarkar9@users.noreply.github.com>
2024-06-16 21:10:44 +0000

    dotnet lts fix

--- test/dotnet/install_dotnet_lts.sh
+++ test/dotnet/install_dotnet_lts.sh
@@ -13,7 +13,7 @@ source dev-container-features-test-lib
 source dotnet_env.sh
 source dotnet_helpers.sh
 
-expected=$(fetch_latest_version_in_channel "LTS")
+expected=$(fetch_latest_version_in_channel "8.0")
 
 check "Latest LTS version installed" \
 is_dotnet_sdk_version_installed "$expected"
