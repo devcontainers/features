3f940f64825b71e17ae4ca88797c125e475ba81c
Richard Höchenberger <richard.hoechenberger@gmail.com>
2024-06-20 10:13:44 +0200

    Betetr comments

--- test/node/debian_yarn_from_corepack.sh
+++ test/node/debian_yarn_from_corepack.sh
@@ -8,10 +8,10 @@ source dev-container-features-test-lib
 # Definition specific tests
 YARN_VERSION="4.3.0"
 
-# Corepack provides shims for package managers like yarn. When yarn is invoked via the "yarn"
+# Corepack provides shims for package managers like yarn. The first time yarn is invoked via the "yarn"
 # command, corepack will interactively request permission to download the yarn binary. To
-# avoid this interactive mode and download the binary automatically, we call "corepack use yarn"
-# instead. Once that command completes, "yarn" can be used normally and in a non-interactive mode.
+# avoid this interactive mode and download the binary automatically, we explicitly call "corepack use yarn"
+# instead (doesn't require user input). Once that command completes, "yarn" can be used in a non-interactive mode.
 check "yarn shim location" bash -c ". /usr/local/share/nvm/nvm.sh && type yarn &> /dev/null"
 check "download yarn" bash -c ". /usr/local/share/nvm/nvm.sh && corepack use yarn@${YARN_VERSION}"
 check "yarn version" bash -c ". /usr/local/share/nvm/nvm.sh && yarn --version | grep ${YARN_VERSION}"
