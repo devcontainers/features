2f64c44e581bec65ee406fdc5a79f678181bb04c
Gaurav Saini <147703805+gauravsaini04@users.noreply.github.com>
2024-07-29 10:07:07 +0000

    [docker-in-docker] - toggle ip6tables settings value as option

--- src/docker-in-docker/install.sh
+++ src/docker-in-docker/install.sh
@@ -20,6 +20,7 @@ INSTALL_DOCKER_COMPOSE_SWITCH="${INSTALLDOCKERCOMPOSESWITCH:-"true"}"
 MICROSOFT_GPG_KEYS_URI="https://packages.microsoft.com/keys/microsoft.asc"
 DOCKER_MOBY_ARCHIVE_VERSION_CODENAMES="bookworm buster bullseye bionic focal jammy noble"
 DOCKER_LICENSED_ARCHIVE_VERSION_CODENAMES="bookworm buster bullseye bionic focal hirsute impish jammy noble"
+TOGGLE_IP6TABLES_SETTING="${IP6TABLES:-"true"}"
 
 # Default: Exit on any failure.
 set -e
