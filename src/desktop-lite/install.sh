c9e7b210b59a4d29c10346070f2204acb7db8d61
Gaurav Saini <147703805+gauravsaini04@users.noreply.github.com>
2024-08-01 16:24:40 +0000

    few more escaped vars - making them unescaped

--- src/desktop-lite/install.sh
+++ src/desktop-lite/install.sh
@@ -309,24 +309,24 @@ export LANGUAGE="${LANGUAGE:-"en_US.UTF-8"}"
 # Execute the command it not already running
 startInBackgroundIfNotRunning()
 {
-    log "Starting \$1."
-    echo -e "\n** \$(date) **" | sudoIf tee -a /tmp/\$1.log > /dev/null
-    if ! pgrep -x \$1 > /dev/null; then
-        keepRunningInBackground "\$@"
-        while ! pgrep -x \$1 > /dev/null; do
+    log "Starting $1."
+    echo -e "\n** $(date) **" | sudoIf tee -a /tmp/\$1.log > /dev/null
+    if ! pgrep -x $1 > /dev/null; then
+        keepRunningInBackground "$@"
+        while ! pgrep -x $1 > /dev/null; do
             sleep 1
         done
-        log "\$1 started."
+        log "$1 started."
     else
-        echo "\$1 is already running." | sudoIf tee -a /tmp/\$1.log > /dev/null
-        log "\$1 is already running."
+        echo "$1 is already running." | sudoIf tee -a /tmp/\$1.log > /dev/null
+        log "$1 is already running."
     fi
 }
 
 # Keep command running in background
 keepRunningInBackground()
 {
-    ($2 bash -c "while :; do echo [\\\$(date)] Process started.; \$3; echo [\\\$(date)] Process exited!; sleep 5; done 2>&1" | sudoIf tee -a /tmp/\$1.log > /dev/null & echo "\$!" | sudoIf tee /tmp/\$1.pid > /dev/null)
+    ($2 bash -c "while :; do echo [\\$(date)] Process started.; $3; echo [\\$(date)] Process exited!; sleep 5; done 2>&1" | sudoIf tee -a /tmp/\$1.log > /dev/null & echo "\$!" | sudoIf tee /tmp/\$1.pid > /dev/null)
 }
 
 # Use sudo to run as root when required
@@ -352,7 +352,7 @@ sudoUserIf()
 # Log messages
 log()
 {
-    echo -e "[\$(date)] \$@" | sudoIf tee -a \$LOG > /dev/null
+    echo -e "[$(date)] $@" | sudoIf tee -a $LOG > /dev/null
 }
 
 log "** SCRIPT START **"
@@ -372,21 +372,21 @@ sudoIf rm -rf /tmp/.X11-unix /tmp/.X*-lock
 mkdir -p /tmp/.X11-unix
 sudoIf chmod 1777 /tmp/.X11-unix
 sudoIf chown root:${group_name} /tmp/.X11-unix
-if [ "\$(echo "\${VNC_RESOLUTION}" | tr -cd 'x' | wc -c)" = "1" ]; then VNC_RESOLUTION=\${VNC_RESOLUTION}x16; fi
-screen_geometry="\${VNC_RESOLUTION%*x*}"
-screen_depth="\${VNC_RESOLUTION##*x}"
+if [ "$(echo "${VNC_RESOLUTION}" | tr -cd 'x' | wc -c)" = "1" ]; then VNC_RESOLUTION=${VNC_RESOLUTION}x16; fi
+screen_geometry="${VNC_RESOLUTION%*x*}"
+screen_depth="${VNC_RESOLUTION##*x}"
 
 # Check if VNC_PASSWORD is set and use the appropriate command
-common_options="tigervncserver \${DISPLAY} -geometry \${screen_geometry} -depth \${screen_depth} -rfbport ${VNC_PORT} -dpi \${VNC_DPI:-96} -localhost -desktop fluxbox -fg"
+common_options="tigervncserver \${DISPLAY} -geometry ${screen_geometry} -depth ${screen_depth} -rfbport ${VNC_PORT} -dpi ${VNC_DPI:-96} -localhost -desktop fluxbox -fg"
 
-if [ -n "\${VNC_PASSWORD+x}" ]; then
-    startInBackgroundIfNotRunning "Xtigervnc" sudoUserIf "\${common_options} -passwd /usr/local/etc/vscode-dev-containers/vnc-passwd"
+if [ -n "${VNC_PASSWORD+x}" ]; then
+    startInBackgroundIfNotRunning "Xtigervnc" sudoUserIf "${common_options} -passwd /usr/local/etc/vscode-dev-containers/vnc-passwd"
 else
-    startInBackgroundIfNotRunning "Xtigervnc" sudoUserIf "\${common_options} -SecurityTypes None"
+    startInBackgroundIfNotRunning "Xtigervnc" sudoUserIf "${common_options} -SecurityTypes None"
 fi
 
 # Spin up noVNC if installed and not running.
-if [ -d "/usr/local/novnc" ] && [ "\$(ps -ef | grep /usr/local/novnc/noVNC*/utils/launch.sh | grep -v grep)" = "" ]; then
+if [ -d "/usr/local/novnc" ] && [ "$(ps -ef | grep /usr/local/novnc/noVNC*/utils/launch.sh | grep -v grep)" = "" ]; then
     keepRunningInBackground "noVNC" sudoIf "/usr/local/novnc/noVNC*/utils/launch.sh --listen ${NOVNC_PORT} --vnc localhost:${VNC_PORT}"
     log "noVNC started."
 else
@@ -395,7 +395,7 @@ fi
 
 # Run whatever was passed in
 log "Executing \"\$@\"."
-exec "\$@"
+exec "$@"
 log "** SCRIPT EXIT **"
 EOF
 
