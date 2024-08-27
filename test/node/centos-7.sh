52c79b4963879dd941c67b583199ec7966e41ab4
Prathamesh Zarkar <159782310+prathameshzarkar9@users.noreply.github.com>
2024-08-28 04:54:29 +0530

    Features update logic for PPA in bionic (#1079)
    * Features update logic for PPA in bionic
    
    * node failing fixed after pnpm install from npm package manager
    
    * for node version 16 pnpm version 7 is installed
    
    * uninstalled pnpm package first and then reinstalled as per node version
    
    * force installing pnpm package
    
    * optimized logic to install compatible pnpm for node 16
    
    * pnpm install specific version compatible for node
    
    * new variable added pnpmVersion in node feature
    
    * test case failure fixed
    
    * fixed test cases
    
    * fixing test cases
    
    * pnpm version fixed for test cases
    
    * default version for pnpm updated
    
    * review comments addressed
    
    * Update src/node/devcontainer-feature.json
    
    ---------
    
    Co-authored-by: Samruddhi Khandale <samruddhikhandale@github.com>

--- test/node/centos-7.sh
+++ test/node/centos-7.sh
@@ -7,7 +7,7 @@ source dev-container-features-test-lib
 
 # Definition specific tests
 check "version" node  --version
-check "pnpm" pnpm -v
+check "pnpm" bash -c "pnpm -v | grep 8.8.0"
 check "nvm" bash -c ". /usr/local/share/nvm/nvm.sh && nvm install 10"
 check "yarn" yarn --version
 
