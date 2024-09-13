c50d982da6f02327551a5f51143c245d985dd0d6
Gaurav Saini <147703805+gauravsaini04@users.noreply.github.com>
2024-09-13 11:12:48 +0000

    err fn addition

--- src/docker-outside-of-docker/install.sh
+++ src/docker-outside-of-docker/install.sh
@@ -27,6 +27,11 @@ set -e
 # Clean up
 rm -rf /var/lib/apt/lists/*
 
+# Setup STDERR.
+err() {
+    echo "(!) $*" >&2
+}
+
 if [ "$(id -u)" -ne 0 ]; then
     echo -e 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
     exit 1
