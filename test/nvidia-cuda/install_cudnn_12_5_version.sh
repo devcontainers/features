7ee889c77471049888cba15cdfb53f8d79d40fc4
Prathamesh Zarkar <159782310+prathameshzarkar9@users.noreply.github.com>
2024-09-25 07:40:16 +0000

    auto find cudnn version

--- test/nvidia-cuda/install_cudnn_12_5_version.sh
+++ test/nvidia-cuda/install_cudnn_12_5_version.sh
@@ -5,12 +5,11 @@ set -e
 # Optional: Import test library
 source dev-container-features-test-lib
 
-# Check installation of libcudnn9-cuda-12 (9.4.0)
+# # Check installation of libcudnn8 (9.4.0)
 check "libcudnn.so.9.4.0" test 1 -eq "$(find /usr -name 'libcudnn.so.9.4.0' | wc -l)"
 
-# Check installation of cuda-nvtx-11-7 (11.7)
-# check "cuda-12-4+nvtx" test -e '/usr/local/cuda-12.4/targets/x86_64-linux/include/nvtx3'
+# Check installation of cuda-nvtx-12-5 (12.5)
+check "cuda-12-5+nvtx" test -e '/usr/local/cuda-12.5/targets/x86_64-linux/include/nvtx3/'
 
 # Report result
 reportResults
-    
