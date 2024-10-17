33294c91df9cf07bb35ed891c9fe98476a6118ee
Prathamesh Zarkar <159782310+prathameshzarkar9@users.noreply.github.com>
2024-10-17 08:41:06 +0000

    removed unused files

--- test/nvidia-cuda/install_cuda_12_4_version.sh
+++ test/nvidia-cuda/install_cuda_12_4_version.sh
@@ -8,7 +8,7 @@ source dev-container-features-test-lib
 # # Check installation of libcudnn8 (9.4.0)
 check "libcudnn.so.9.4.0" test 1 -eq "$(find /usr -name 'libcudnn.so.9.4.0' | wc -l)"
 
-# Check installation of cuda-nvtx-12-3 (12.3)
+# Check installation of cuda-nvtx-12-4 (12.4)
 check "cuda-12-4+nvtx" test -e '/usr/local/cuda-12.4/targets/x86_64-linux/include/nvtx3/'
 
 # Report result
