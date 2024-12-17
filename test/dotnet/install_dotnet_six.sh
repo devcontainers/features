68ae7cb8c9c820c5ab519b039d14ced7bfc4103d
Kaniska244 <kaniska244@github.com>
2024-12-17 10:19:28 +0000

    Added test to see if dotnet 6.0 still works

--- test/dotnet/install_dotnet_six.sh
+++ test/dotnet/install_dotnet_six.sh
@@ -13,18 +13,15 @@ source dev-container-features-test-lib
 source dotnet_env.sh
 source dotnet_helpers.sh
 
-check ".NET SDK 8.0 installed" \
-is_dotnet_sdk_version_installed "8.0"
+expected=$(fetch_latest_version_in_channel "6.0")
 
-check ".NET SDK 7.0 installed" \
-is_dotnet_sdk_version_installed "7.0"
+check ".NET Core SDK 6.0 installed" \
+is_dotnet_sdk_version_installed "$expected"
 
-check ".NET SDK 6.0 installed" \
-is_dotnet_sdk_version_installed "6.0"
-
-check "Build example class library" \
-dotnet build projects/multitargeting
+check "Build and run example project" \
+dotnet run --project projects/net6.0 
 
 # Report results
 # If any of the checks above exited with a non-zero exit code, the test will fail.
 reportResults
+
