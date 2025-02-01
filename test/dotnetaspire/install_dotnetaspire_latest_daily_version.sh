e0fbae485008a70a61848189266666e289959b9d
Dan Moseley <danmose@microsoft.com>
2025-01-31 20:04:40 -0700

    rest of tests

--- test/dotnetaspire/install_dotnetaspire_latest_daily_version.sh
+++ test/dotnetaspire/install_dotnetaspire_latest_daily_version.sh
@@ -2,8 +2,6 @@
 
 set -e
 
-# Run tests with `devcontainer features test -f dotnetaspire` in the parent of the src and test folders.
-
 # Optional: Import test library bundled with the devcontainer CLI
 # See https://github.com/devcontainers/cli/blob/HEAD/docs/features/test.md#dev-container-features-test-lib
 # Provides the 'check' and 'reportResults' commands.
@@ -14,13 +12,6 @@ source dev-container-features-test-lib
 # check <LABEL> <cmd> [args...]
 source dotnet_env.sh
 
-check "dotnet is installed in DOTNET_ROOT and execute permission is granted" \
-test -x "$DOTNET_ROOT/dotnet" 
-
-check "dotnetaspire templates are installed" \
-test "$DOTNET_ROOT/dotnet new aspire"
-
-# There isn't currently a good way to check what version of the templates was installed.
 
 # Report results
 # If any of the checks above exited with a non-zero exit code, the test will fail.
