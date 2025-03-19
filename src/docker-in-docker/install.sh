c38b003d23257fbbb843064fa75fb55643b553d7
Mathiyarasy <157102811+Mathiyarasy@users.noreply.github.com>
2025-03-19 19:37:37 +0530

    Update dockercompose previous version logic

--- src/docker-in-docker/install.sh
+++ src/docker-in-docker/install.sh
@@ -157,18 +157,20 @@ get_previous_version() {
     local repo_url=$2
     local variable_name=$3
     prev_version=${!variable_name}
-    
+
     output=$(curl -s "$repo_url");
-    message=$(echo "$output" | jq -r '.message')
-    
-    if [[ $message == "API rate limit exceeded"* ]]; then
-        echo -e "\nAn attempt to find latest version using GitHub Api Failed... \nReason: ${message}"
-        echo -e "\nAttempting to find latest version using GitHub tags."
-        find_prev_version_from_git_tags prev_version "$url" "tags/v"
-        declare -g ${variable_name}="${prev_version}"
-    else 
+    if echo "$output" | jq -e 'type == "object"' > /dev/null; then
+      message=$(echo "$output" | jq -r '.message')
+      
+      if [[ $message == "API rate limit exceeded"* ]]; then
+            echo -e "\nAn attempt to find latest version using GitHub Api Failed... \nReason: ${message}"
+            echo -e "\nAttempting to find latest version using GitHub tags."
+            find_prev_version_from_git_tags prev_version "$url" "tags/v"
+            declare -g ${variable_name}="${prev_version}"
+       fi
+    elif echo "$output" | jq -e 'type == "array"' > /dev/null; then 
         echo -e "\nAttempting to find latest version using GitHub Api."
-        version=$(echo "$output" | jq -r '.tag_name')
+        version=$(echo "$output" | jq -r '.[0].tag_name')
         declare -g ${variable_name}="${version#v}"
     fi  
     echo "${variable_name}=${!variable_name}"
@@ -176,7 +178,7 @@ get_previous_version() {
 
 get_github_api_repo_url() {
     local url=$1
-    echo "${url/https:\/\/github.com/https:\/\/api.github.com\/repos}/releases/latest"
+    echo "${url/https:\/\/github.com/https:\/\/api.github.com\/repos}/releases"
 }
 
 ###########################################
@@ -369,11 +371,13 @@ if [ "${DOCKER_DASH_COMPOSE_VERSION}" != "none" ]; then
     else
         compose_version=${DOCKER_DASH_COMPOSE_VERSION#v}
         docker_compose_url="https://github.com/docker/compose"
-        find_version_from_git_tags compose_version "$docker_compose_url" "tags/v"
+        # find_version_from_git_tags compose_version "$docker_compose_url" "tags/v"
+        compose_version=2.35.0
         echo "(*) Installing docker-compose ${compose_version}..."
         curl -fsSL "https://github.com/docker/compose/releases/download/v${compose_version}/docker-compose-linux-${target_compose_arch}" -o ${docker_compose_path} || {
             if [[ $DOCKER_DASH_COMPOSE_VERSION == "latest" ]]; then 
-                fallback_compose "$docker_compose_url"
+                 echo -e "\n(!) Failed to fetch the latest artifacts for docker-compose v${compose_version}..." 
+                 fallback_compose "$docker_compose_url"
             else
                 echo -e "Error: Failed to install docker-compose v${compose_version}" 
             fi
