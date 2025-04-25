9ae64f34c16f3be4db8df478fcef92cf91322e65
Kaniska <kaniska244@github.com>
2025-04-25 14:07:56 +0000

    Fixing failing tests

--- test/node/install_additional_node.sh
+++ test/node/install_additional_node.sh
@@ -6,7 +6,7 @@ set -e
 source dev-container-features-test-lib
 
 # 'lts' is now some version of node 20...
-check "version_on_path"  node -v | grep 20
+check "version_on_path"  node -v | grep 22
 check "pnpm" bash -c "pnpm -v | grep 8.8.0"
 
 check "v20_installed" ls -1 /usr/local/share/nvm/versions/node | grep 20
