69dd8bd4d389fc0cbad6a4104edbf4dbf644f3a4
sireeshajonnalagadda <sireeshajonnalagadda@github.com>
2025-04-29 10:45:16 +0000

    shell linter check

--- src/docker-in-docker/install.sh
+++ src/docker-in-docker/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#shellcheck disable=all
+# shellcheck disable=all
 #-------------------------------------------------------------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See https://go.microsoft.com/fwlink/?linkid=2090316 for license information.
