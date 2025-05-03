25fc94945f382a885b3337a917748a45042afd96
Randall <me@randall.network>
2025-05-02 17:43:36 -0700

    test(anaconda): :white_check_mark: add write permission checks to test scripts

--- test/anaconda/default.sh
+++ test/anaconda/default.sh
@@ -15,5 +15,10 @@ check "notice file exists" test -f /usr/local/etc/vscode-dev-containers/conda-no
 check "conda-forge channel" conda config --show channels | grep conda-forge
 check "mamba installed" conda list -n base | grep mamba
 
+# Test write access to the Conda environment
+check "conda-update-conda" bash -c "conda update -y conda"
+check "conda-install-tensorflow" bash -c "conda create --name test-env -c conda-forge --yes tensorflow"
+check "conda-install-pytorch" bash -c "conda create --name test-env2 -c conda-forge --yes pytorch"
+
 # Report results
 reportResults
