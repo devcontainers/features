7adf28c8af9d4b979a79ed219d07f264768da783
Kaniska <kaniska244@github.com>
2025-05-27 14:53:49 +0000

    Version bump and dotnet rollback.

--- test/dotnet/install_dotnet_multiple_versions.sh
+++ test/dotnet/install_dotnet_multiple_versions.sh
@@ -22,6 +22,15 @@ is_dotnet_sdk_version_installed "8.0"
 check ".NET SDK 7.0 installed" \
 is_dotnet_sdk_version_installed "7.0"
 
+check ".NET SDK 6.0 installed" \
+is_dotnet_sdk_version_installed "6.0"
+
+check ".NET SDK 5.0 installed" \
+is_dotnet_sdk_version_installed "5.0"
+
+check ".NET Core SDK 3.1 installed" \
+is_dotnet_sdk_version_installed "3.1"
+
 check "Build example class library" \
 dotnet build projects/multitargeting
 
@@ -33,6 +42,15 @@ dotnet run --project projects/net8.0
 
 check "Build and run .NET 7.0 project" \
 dotnet run --project projects/net7.0
+
+check "Build and run .NET 6.0 project" \
+dotnet run --project projects/net6.0
+
+check "Build and run .NET 5.0 project" \
+dotnet run --project projects/net5.0
+
+check "Build and run .NET Core 3.1 project" \
+dotnet run --project projects/netcoreapp3.1
 
 # Report results
 # If any of the checks above exited with a non-zero exit code, the test will fail.
