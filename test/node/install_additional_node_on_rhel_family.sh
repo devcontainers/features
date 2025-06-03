c10318451797b818c18e39cf60e2c2f242a25ae8
Mathiyarasy <157102811+Mathiyarasy@users.noreply.github.com>
2025-06-03 10:29:27 +0000

    Remove hardcoded node lts version from test scripts

--- test/node/install_additional_node_on_rhel_family.sh
+++ test/node/install_additional_node_on_rhel_family.sh
@@ -5,11 +5,19 @@ set -e
 # Optional: Import test library
 source dev-container-features-test-lib
 
-# 'lts' is now some version of node 20...
-check "version_on_path"  node -v | grep 20
+# Install jq if not already installed
+if ! type jq >/dev/null 2>&1; then
+    yum install -y jq
+fi 
+
+#Get the latest LTS version of Node.js
+LATEST_LTS_VERSION=$(curl -s https://nodejs.org/dist/index.json | jq -r '[.[] | select(.lts != false)][0].version')
+
+# 'lts' is fetched instead of hardcoded to a specific version
+check "version_on_path"  node -v | grep "$LATEST_LTS_VERSION"
 check "pnpm" bash -c "pnpm -v | grep 6.16.0"
 
-check "v20_installed" ls -1 /usr/local/share/nvm/versions/node | grep 20
+check "lts_installed" ls -1 /usr/local/share/nvm/versions/node | grep "$LATEST_LTS_VERSION"
 check "v14_installed" ls -1 /usr/local/share/nvm/versions/node | grep 14.19.3
 check "v17_installed" ls -1 /usr/local/share/nvm/versions/node | grep 17.9.1
 
