d5d5573740c6e7ee20c09c227da2b3e9e1f2b58c
Mathiyarasy <157102811+Mathiyarasy@users.noreply.github.com>
2025-06-04 10:22:13 +0000

    update test cases to check for specific versions

--- test/kubectl-helm-minikube/install_kubectl_with_version.sh
+++ test/kubectl-helm-minikube/install_kubectl_with_version.sh
@@ -4,24 +4,29 @@ set -e
 # Optional: Import test library
 source dev-container-features-test-lib
 
+# Define expected versions
+KUBECTL_EXPECTED_VERSION="v1.33.0"
+HELM_VERSION="v3.17.3"
+MINIKUBE_VERSION="v1.31.1"
+
 set +e
-    kubectl
+    kubectl version --client --output json | jq -r '.clientVersion.gitVersion' | grep "${KUBECTL_VERSION}"
     exit_code=$?
-    check "kubectl-is-installed" bash -c "echo ${exit_code} | grep 0"
+    check "kubectl-version-${KUBECTL_VERSION}-installed" bash -c "echo ${exit_code} | grep 0"
     echo "kubectl version:"
     kubectl version --client 
 
-    helm version
+    helm version --short | grep "${HELM_VERSION}"
     exit_code=$?
-    check "helm-is-installed" bash -c "echo ${exit_code} | grep 0"
+    check "helm-version-${HELM_VERSION}-installed" bash -c "echo ${exit_code} | grep 0"
     echo "helm version:"
-    helm version 
+    helm version --short
 
-    minikube version
+    minikube version --short | grep "${MINIKUBE_VERSION}"  
     exit_code=$?
-    check "minikube-is-installed" bash -c "echo ${exit_code} | grep 0"
+    check "minikube-version-${MINIKUBE_VERSION}-installed" bash -c "echo ${exit_code} | grep 0"
     echo "minikube version:"
-    minikube version
+    minikube version --short
 set -e
 
 # Report result
--- test/kubectl-helm-minikube/install_kubectl_without_version.sh
+++ test/kubectl-helm-minikube/install_kubectl_without_version.sh
@@ -4,24 +4,29 @@ set -e
 # Optional: Import test library
 source dev-container-features-test-lib
 
+# Define expected versions
+KUBECTL_EXPECTED_VERSION="1.30"
+HELM_VERSION="3.16"
+MINIKUBE_VERSION="1.28"
+
 set +e
-    kubectl
+    kubectl version --client --output json | jq -r '.clientVersion.gitVersion' | grep "${KUBECTL_VERSION}"
     exit_code=$?
-    check "kubectl-is-installed" bash -c "echo ${exit_code} | grep 0"
+    check "kubectl-version-${KUBECTL_VERSION}-installed" bash -c "echo ${exit_code} | grep 0"
     echo "kubectl version:"
     kubectl version --client 
 
-    helm version
+    helm version --short | grep "${HELM_VERSION}"
     exit_code=$?
-    check "helm-is-installed" bash -c "echo ${exit_code} | grep 0"
+    check "helm-version-${HELM_VERSION}-installed" bash -c "echo ${exit_code} | grep 0"
     echo "helm version:"
-    helm version 
+    helm version --short
 
-    minikube version
+    minikube version --short | grep "${MINIKUBE_VERSION}"  
     exit_code=$?
-    check "minikube-is-installed" bash -c "echo ${exit_code} | grep 0"
+    check "minikube-version-${MINIKUBE_VERSION}-installed" bash -c "echo ${exit_code} | grep 0"
     echo "minikube version:"
-    minikube version
+    minikube version --short
 set -e
 
 # Report result
