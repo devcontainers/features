2958fdbbd1e139f542f6bb12feb3eb195eacdcb8
sireeshajonnalagadda <sireeshajonnalagadda@github.com>
2025-06-11 10:10:28 +0000

    reverted the changes for better readability

--- src/docker-in-docker/install.sh
+++ src/docker-in-docker/install.sh
@@ -7,8 +7,8 @@
 # Docs: https://github.com/microsoft/vscode-dev-containers/blob/main/script-library/docs/docker-in-docker.md
 # Maintainer: The Dev Container spec maintainers
 if [ -z "$BASH_VERSION" ]; then
-echo "❌ This script must be run with bash, not sh."
-exit 1
+    echo "❌ This script must be run with bash, not sh."
+    exit 1
 fi
 DOCKER_VERSION="${VERSION:-"latest"}" # The Docker/Moby Engine + CLI should match in version
 USE_MOBY="${MOBY:-"true"}"
@@ -36,8 +36,8 @@ echo "(!) $*" >&2
 }
 
 if [ "$(id -u)" -ne 0 ]; then
-err 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
-exit 1
+    err 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
+    exit 1
 fi
 
 ###################
@@ -47,12 +47,12 @@ fi
 
 # Determine the appropriate non-root user
 if [ "${USERNAME}" = "auto" ] || [ "${USERNAME}" = "automatic" ]; then
-USERNAME=""
-POSSIBLE_USERS=("vscode" "node" "codespace" "$(awk -v val=1000 -F ":" '$3==val{print $1}' /etc/passwd)")
-for CURRENT_USER in "${POSSIBLE_USERS[@]}"; do
+    USERNAME=""
+    POSSIBLE_USERS=("vscode" "node" "codespace" "$(awk -v val=1000 -F ":" '$3==val{print $1}' /etc/passwd)")
+    for CURRENT_USER in "${POSSIBLE_USERS[@]}"; do
 if id -u ${CURRENT_USER} > /dev/null 2>&1; then
-USERNAME=${CURRENT_USER}
-break
+    USERNAME=${CURRENT_USER}
+    break
 fi
 done
 if [ "${USERNAME}" = "" ]; then
@@ -66,10 +66,10 @@ apt_get_update()
 {
 . /etc/os-release
 if [ "$ID" = "ubuntu" ] || [ "$ID" = "debian" ]; then
-if [ "$(find /var/lib/apt/lists/* | wc -l)" = "0" ]; then
-echo "Running apt-get update..."
-apt-get update -y
-fi
+    if [ "$(find /var/lib/apt/lists/* | wc -l)" = "0" ]; then
+        echo "Running apt-get update..."
+        apt-get update -y
+    fi
 fi
 }
 
@@ -77,134 +77,133 @@ fi
 check_packages() {
 
 if [ "$ID" = "ubuntu" ] || [ "$ID" = "debian" ] || [ "$ID_LIKE" = "debian" ]; then
-if ! dpkg -s "$@" > /dev/null 2>&1; then
-apt_get_update
-apt-get -y install --no-install-recommends "$@"
-fi
+    if ! dpkg -s "$@" > /dev/null 2>&1; then
+        apt_get_update
+        apt-get -y install --no-install-recommends "$@"
+    fi
 elif [ "$ID" = "fedora" ] || [ "$ID" = "centos" ] || [ "$ID_LIKE" == "rhel" ]; then
-if ! dnf list installed "$@" > /dev/null 2>&1; then
-dnf -y install "$@"
-fi
+    if ! dnf list installed "$@" > /dev/null 2>&1; then
+        dnf -y install "$@"
+    fi
 fi
 
 }
 missing=0
 for cmd in git wget which; do
-command -v $cmd &>/dev/null || {
-echo "$cmd not found"
-missing=1
-}
+    command -v $cmd &>/dev/null || {
+    echo "$cmd not found"
+    missing=1
+    }
 done
 
 if [ $missing -eq 1 ]; then
-echo "Installing missing packages..."
-if command -v dnf &>/dev/null; then
-dnf install -y git wget which curl jq
-elif command -v apt &>/dev/null; then
-apt-get update && apt-get install -y git wget curl jq
-else
-echo "Unsupported package manager"
-exit 1
-fi
+    echo "Installing missing packages..."
+    if command -v dnf &>/dev/null; then
+        dnf install -y git wget which curl jq
+        elif command -v apt &>/dev/null; then
+            apt-get update && apt-get install -y git wget curl jq
+            else
+                echo "Unsupported package manager"
+                exit 1
+    fi
 fi
 
 
 # Figure out correct version of a three part version number is not passed
 find_version_from_git_tags() {
-local variable_name=$1
-local requested_version=${!variable_name}
-if [ "${requested_version}" = "none" ]; then return; fi
-local repository=$2
-local prefix=${3:-"tags/v"}
-local separator=${4:-"."}
-local last_part_optional=${5:-"false"}
-if [ "$(echo "${requested_version}" | grep -o "." | wc -l)" != "2" ]; then
-local escaped_separator=${separator//./\\.}
-local last_part
-if [ "${last_part_optional}" = "true" ]; then
-last_part="(${escaped_separator}[0-9]+)?"
-else
-last_part="${escaped_separator}[0-9]+"
-fi
-local regex="${prefix}\\K[0-9]+${escaped_separator}[0-9]+${last_part}$"
-local version_list="$(git ls-remote --tags ${repository} | grep -oP "${regex}" | tr -d ' ' | tr "${separator}" "." | sort -rV)"
-if [ "${requested_version}" = "latest" ] || [ "${requested_version}" = "current" ] || [ "${requested_version}" = "lts" ]; then
-declare -g ${variable_name}="$(echo "${version_list}" | head -n 1)"
-else
-set +e
-declare -g ${variable_name}="$(echo "${version_list}" | grep -E -m 1 "^${requested_version//./\\.}([\\.\\s]|$)")"
-set -e
-fi
-fi
-if [ -z "${!variable_name}" ] || ! echo "${version_list}" | grep "^${!variable_name//./\\.}$" > /dev/null 2>&1; then
-err "Invalid ${variable_name} value: ${requested_version}\nValid values:\n${version_list}" >&2
-exit 1
-fi
-echo "${variable_name}=${!variable_name}"
+    local variable_name=$1
+    local requested_version=${!variable_name}
+    if [ "${requested_version}" = "none" ]; then return; fi
+    local repository=$2
+    local prefix=${3:-"tags/v"}
+    local separator=${4:-"."}
+    local last_part_optional=${5:-"false"}
+    if [ "$(echo "${requested_version}" | grep -o "." | wc -l)" != "2" ]; then
+        local escaped_separator=${separator//./\\.}
+        local last_part
+        if [ "${last_part_optional}" = "true" ]; then
+            last_part="(${escaped_separator}[0-9]+)?"
+        else
+            last_part="${escaped_separator}[0-9]+"
+        fi
+        local regex="${prefix}\\K[0-9]+${escaped_separator}[0-9]+${last_part}$"
+        local version_list="$(git ls-remote --tags ${repository} | grep -oP "${regex}" | tr -d ' ' | tr "${separator}" "." | sort -rV)"
+        if [ "${requested_version}" = "latest" ] || [ "${requested_version}" = "current" ] || [ "${requested_version}" = "lts" ]; then
+            declare -g ${variable_name}="$(echo "${version_list}" | head -n 1)"
+        else
+            set +e
+            declare -g ${variable_name}="$(echo "${version_list}" | grep -E -m 1 "^${requested_version//./\\.}([\\.\\s]|$)")"
+            set -e
+        fi
+    fi
+    if [ -z "${!variable_name}" ] || ! echo "${version_list}" | grep "^${!variable_name//./\\.}$" > /dev/null 2>&1; then
+        err "Invalid ${variable_name} value: ${requested_version}\nValid values:\n${version_list}" >&2
+        exit 1
+    fi
+    echo "${variable_name}=${!variable_name}"
 }
 
 # Use semver logic to decrement a version number then look for the closest match
 find_prev_version_from_git_tags() {
-local variable_name=$1
-local current_version=${!variable_name}
-local repository=$2
-# Normally a "v" is used before the version number, but support alternate cases
-local prefix=${3:-"tags/v"}
-# Some repositories use "_" instead of "." for version number part separation, support that
-local separator=${4:-"."}
-# Some tools release versions that omit the last digit (e.g. go)
-local last_part_optional=${5:-"false"}
-# Some repositories may have tags that include a suffix (e.g. actions/node-versions)
-local version_suffix_regex=$6
-# Try one break fix version number less if we get a failure. Use "set +e" since "set -e" can cause failures in valid scenarios.
-set +e
-major="$(echo "${current_version}" | grep -oE '^[0-9]+' || echo '')"
-minor="$(echo "${current_version}" | grep -oP '^[0-9]+\.\K[0-9]+' || echo '')"
-breakfix="$(echo "${current_version}" | grep -oP '^[0-9]+\.[0-9]+\.\K[0-9]+' 2>/dev/null || echo '')"
+    local variable_name=$1
+    local current_version=${!variable_name}
+    local repository=$2
+    # Normally a "v" is used before the version number, but support alternate cases
+    local prefix=${3:-"tags/v"}
+    # Some repositories use "_" instead of "." for version number part separation, support that
+    local separator=${4:-"."}
+    # Some tools release versions that omit the last digit (e.g. go)
+    local last_part_optional=${5:-"false"}
+    # Some repositories may have tags that include a suffix (e.g. actions/node-versions)
+    local version_suffix_regex=$6
+    # Try one break fix version number less if we get a failure. Use "set +e" since "set -e" can cause failures in valid scenarios.
+    set +e
+    major="$(echo "${current_version}" | grep -oE '^[0-9]+' || echo '')"
+    minor="$(echo "${current_version}" | grep -oP '^[0-9]+\.\K[0-9]+' || echo '')"
+    breakfix="$(echo "${current_version}" | grep -oP '^[0-9]+\.[0-9]+\.\K[0-9]+' 2>/dev/null || echo '')"
 
-if [ "${minor}" = "0" ] && [ "${breakfix}" = "0" ]; then
-((major=major-1))
-declare -g ${variable_name}="${major}"
-# Look for latest version from previous major release
-find_version_from_git_tags "${variable_name}" "${repository}" "${prefix}" "${separator}" "${last_part_optional}"
-# Handle situations like Go's odd version pattern where "0" releases omit the last part
-elif [ "${breakfix}" = "" ] || [ "${breakfix}" = "0" ]; then
-((minor=minor-1))
-declare -g ${variable_name}="${major}.${minor}"
-# Look for latest version from previous minor release
-find_version_from_git_tags "${variable_name}" "${repository}" "${prefix}" "${separator}" "${last_part_optional}"
-else
-((breakfix=breakfix-1))
-if [ "${breakfix}" = "0" ] && [ "${last_part_optional}" = "true" ]; then
-declare -g ${variable_name}="${major}.${minor}"
-else 
-declare -g ${variable_name}="${major}.${minor}.${breakfix}"
-fi
-fi
-set -e
+    if [ "${minor}" = "0" ] && [ "${breakfix}" = "0" ]; then
+        ((major=major-1))
+        declare -g ${variable_name}="${major}"
+        # Look for latest version from previous major release
+        find_version_from_git_tags "${variable_name}" "${repository}" "${prefix}" "${separator}" "${last_part_optional}"
+        # Handle situations like Go's odd version pattern where "0" releases omit the last part
+    elif [ "${breakfix}" = "" ] || [ "${breakfix}" = "0" ]; then
+        ((minor=minor-1))
+        declare -g ${variable_name}="${major}.${minor}"
+        # Look for latest version from previous minor release
+        find_version_from_git_tags "${variable_name}" "${repository}" "${prefix}" "${separator}" "${last_part_optional}"
+    else
+        ((breakfix=breakfix-1))
+        if [ "${breakfix}" = "0" ] && [ "${last_part_optional}" = "true" ]; then
+            declare -g ${variable_name}="${major}.${minor}"
+        else 
+            declare -g ${variable_name}="${major}.${minor}.${breakfix}"
+        fi
+    fi
+    set -e
 }
 
 # Function to fetch the version released prior to the latest version
 get_previous_version() {
-local url=$1
-local repo_url=$2
-local variable_name=$3
-prev_version=${!variable_name}
-
-output=$(curl -s "$repo_url");
+    local url=$1
+    local repo_url=$2
+    local variable_name=$3
+    prev_version=${!variable_name}
+    output=$(curl -s "$repo_url");
 if echo "$output" | jq -e 'type == "object"' > /dev/null; then
-message=$(echo "$output" | jq -r '.message')
+    message=$(echo "$output" | jq -r '.message')
 
 if [[ $message == "API rate limit exceeded"* ]]; then
-echo -e "\nAn attempt to find latest version using GitHub Api Failed... \nReason: ${message}"
-echo -e "\nAttempting to find latest version using GitHub tags."
-find_prev_version_from_git_tags prev_version "$url" "tags/v"
-declare -g ${variable_name}="${prev_version}"
+    echo -e "\nAn attempt to find latest version using GitHub Api Failed... \nReason: ${message}"
+    echo -e "\nAttempting to find latest version using GitHub tags."
+    find_prev_version_from_git_tags prev_version "$url" "tags/v"
+    declare -g ${variable_name}="${prev_version}"
 fi
 elif echo "$output" | jq -e 'type == "array"' > /dev/null; then 
-echo -e "\nAttempting to find latest version using GitHub Api."
-version=$(echo "$output" | jq -r '.[1].tag_name')
-declare -g ${variable_name}="${version#v}"
+    echo -e "\nAttempting to find latest version using GitHub Api."
+    version=$(echo "$output" | jq -r '.[1].tag_name')
+    declare -g ${variable_name}="${version#v}"
 fi  
 echo "${variable_name}=${!variable_name}"
 }
@@ -226,19 +225,18 @@ export DEBIAN_FRONTEND=noninteractive
 . /etc/os-release
 # Fetch host/container architecture
 if [ "$ID" = "ubuntu" ] || [ "$ID" = "debian" ]; then
-architecture="$(dpkg --print-architecture)"
+    architecture="$(dpkg --print-architecture)"
 elif [ "$ID" = "fedora" ] || [ "$ID_LIKE" = "rhel" ]; then
-architecture="$(uname -m)"
-case "$architecture" in
-x86_64) architecture="amd64" ;;
-aarch64) architecture="arm64" ;;
-*) echo "Unsupported architecture: $architecture"; exit 1 ;;
-esac
+    architecture="$(uname -m)"
+    case "$architecture" in
+    x86_64) architecture="amd64" ;;
+    aarch64) architecture="arm64" ;;
+    *) echo "Unsupported architecture: $architecture"; exit 1 ;;
+    esac
 else
-echo "Unsupported operating system: $ID"
-exit 1
+    echo "Unsupported operating system: $ID"
+    exit 1
 fi
-
 
 # Check if distro is supported
 if [ "${USE_MOBY}" = "true" ]; then
