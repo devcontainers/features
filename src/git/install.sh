809a7f1f76a2d6d25bd5c024a4ea3a87f8c6ea8f
Eimhin Laverty <eimhin.laverty@rover.com>
2025-07-18 09:39:56 +0100

    Provide error response when failure occurs fetching Git tags

--- src/git/install.sh
+++ src/git/install.sh
@@ -288,7 +288,13 @@ fi
 # Partial version matching
 if [ "$(echo "${GIT_VERSION}" | grep -o '\.' | wc -l)" != "2" ]; then
     requested_version="${GIT_VERSION}"
-    version_list="$(curl -sSL -H "Accept: application/vnd.github.v3+json" "https://api.github.com/repos/git/git/tags" | grep -oP '"name":\s*"v\K[0-9]+\.[0-9]+\.[0-9]+"' | tr -d '"' | sort -rV )"
+    version_response="$(curl --fail-with-body -sSL -H "Accept: application/vnd.github.v3+json" "https://api.github.com/repos/git/git/tags")"
+    if [ $? != 0 ]; then
+        echo "Failed to fetch git versions from GitHub API:" >&2
+        echo "$version_response" >&2
+        exit 1
+    fi
+    version_list="$(echo "$version_response" | grep -oP '"name":\s*"v\K[0-9]+\.[0-9]+\.[0-9]+"' | tr -d '"' | sort -rV )"
     if [ "${requested_version}" = "latest" ] || [ "${requested_version}" = "lts" ] || [ "${requested_version}" = "current" ]; then
         GIT_VERSION="$(echo "${version_list}" | head -n 1)"
     else
