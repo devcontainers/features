42dcd87077e1a21023ac779f361c1853fd3f553b
Kaniska <kaniska244@github.com>
2025-07-24 14:18:26 +0000

    Small changes in the test scripts.

--- test/terraform/install_in_ubuntu_noble.sh
+++ test/terraform/install_in_ubuntu_noble.sh
@@ -5,6 +5,9 @@ set -e
 # Import test library
 source dev-container-features-test-lib
 
+# Check to make sure the user is vscode
+check "user is vscode" whoami | grep vscode
+
 # Check if terraform was installed correctly
 check "terraform installed" terraform --version
 
