dcd0c88e29623fabf3c9fd26c42b4c0e2e6692e8
Mathiyarasy <157102811+Mathiyarasy@users.noreply.github.com>
2025-10-06 06:02:48 +0000

    update version

--- test/azure-cli/install_azcli_dotnet_dockerindocker_trixie.sh
+++ test/azure-cli/install_azcli_dotnet_dockerindocker_trixie.sh
@@ -5,11 +5,9 @@ set -e
 # Import test library for `check` command
 source dev-container-features-test-lib
 
-# Check to make sure the user is vscode
-check "user is vscode" whoami | grep vscode
-
 check "version" az  --version
 
+check "docker installed" bash -c "type docker"
 
 # Report result
 reportResults
