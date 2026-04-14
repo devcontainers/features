ac2413713e37f570c32d82f43c4eca24be16e5f0
Kaniska <kaniska244@github.com>
2026-04-14 19:13:03 +0530

    Move conda installation from deprecated DEBs to Miniconda installer (#1619)
    * Move conda installation from deprecated DEBs to Miniconda installer
    
    * Fix missing newline at end of conda_channel_creation.sh
    
    * Fix missing newline at end of install_conda_package_after_upgrade.sh
    
    ---------
    
    Co-authored-by: Abdurrahmaan Iqbal <abdurriq@github.com>

--- test/conda/conda_channel_creation.sh
+++ test/conda/conda_channel_creation.sh
@@ -7,8 +7,8 @@ source dev-container-features-test-lib
 
 ## Test Conda
 check "conda-update-conda" bash -c "conda update -c defaults -y conda"
-check "conda-install-tensorflow" bash -c "conda create --name test-env -c conda-forge --yes tensorflow"
-check "conda-install-pytorch" bash -c "conda create --name test-env -c conda-forge --yes pytorch"
+check "conda-install-tensorflow" bash -c "conda create --name tensorflow-test-env -c conda-forge --yes tensorflow"
+check "conda-install-pytorch" bash -c "conda create --name pytorch-test-env -c conda-forge --yes pytorch"
 
 # Report result
 reportResults
--- test/conda/install_conda_package_after_upgrade.sh
+++ test/conda/install_conda_package_after_upgrade.sh
@@ -9,8 +9,11 @@ source dev-container-features-test-lib
 # This validates the fix for the pluggy/conda version mismatch issue where
 # conda self-upgrades but the older pluggy lacks the 'wrapper' attribute
 check "conda version" conda --version
+check "conda update" conda update -n base -c defaults -y conda
+check "conda updated version" conda --version
 check "install pyopenssl" conda install -y -c defaults pyopenssl
 check "install cryptography" conda install -y -c defaults cryptography
+check "install scipy with bioconda" conda install -y -c bioconda scipy
 check "conda-forge" conda config --show channels | grep conda-forge
 check "if conda-notice.txt exists" cat /usr/local/etc/vscode-dev-containers/conda-notice.txt
 
