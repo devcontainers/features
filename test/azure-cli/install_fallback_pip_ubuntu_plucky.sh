923675255a1bb611f0622c985c3930a0195bd75a
Kaniska <kaniska244@github.com>
2026-05-28 10:37:06 +0000

    Correcting the fallback test

--- test/azure-cli/install_fallback_pip_ubuntu_plucky.sh
+++ test/azure-cli/install_fallback_pip_ubuntu_plucky.sh
@@ -5,9 +5,6 @@ set -e
 # Import test library for `check` command
 source dev-container-features-test-lib
 
-# Check to make sure the user is vscode
-check "user is vscode" whoami | grep vscode
-
 # Ubuntu plucky (25.04) is NOT in the apt archive codename allowlist,
 # so this test validates the pip fallback path.
 check "version" az  --version
