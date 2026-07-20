c6747068361551ff296b05b99409be64782db735
Venkumahanti Subhankar <v-subhankar-infy@github.com>
2026-07-20 18:51:47 +0530

    Fix string quotes in install_latest_version.sh

--- test/java/install_latest_version.sh
+++ test/java/install_latest_version.sh
@@ -8,7 +8,7 @@ source dev-container-features-test-lib
 echo 'public class HelloWorld { public static void main(String[] args) { System.out.println("Hello, World!"); } }' > HelloWorld.java
 javac HelloWorld.java
 
-check "hello world" /bin/bash -c "java HelloWorld | grep "Hello, World!""
+check "hello world" /bin/bash -c 'java HelloWorld | grep "Hello, World!"'
 check "java version latest installed" grep "26" <(java --version)
 
 # Report result
